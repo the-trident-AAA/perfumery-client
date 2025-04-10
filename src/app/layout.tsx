eb7dfca325c7407b4e2f18dbf0fd5343fb3e2f25
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-10 17:58:00 -0400

    feat(home): added offer section in home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,13 @@ import "../ui/globals.css";
 import { roboto } from "../ui/fonts";
 import Header from "../sections/root-layout/components/header/header";
 
-
-
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <html>
       <body className={`${roboto.className} antialiased`}>
         <main className="flex min-h-screen flex-col">
           <Header />
-          {children}
+          <div className="container mx-auto mt-5">{children}</div>
         </main>
       </body>
     </html>
