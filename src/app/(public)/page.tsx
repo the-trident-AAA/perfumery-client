41d6133e2d2e0714d7f9fef920289ec47ab8a083
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-10 19:19:29 -0400

    feat(app): added product-card and create test-card page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,6 @@
-import HomeContainer from '@/src/sections/home/home-container'
-import React from 'react'
+import HomeContainer from "@/src/sections/home/home-container";
+import React from "react";
 
 export default function HomePage() {
-  return (
-    <HomeContainer />
-  )
+  return <HomeContainer />;
 }
