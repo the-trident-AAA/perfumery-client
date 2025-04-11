471bbefabeacbb2d8cb02b7bea9ae983c17156a1
Pelaeezzz <sharkpelaez@gmail.com>
2025-04-11 18:26:43 -0400

    feat(app) added best-selling perfumes

--- src/sections/home/home-container.tsx
+++ src/sections/home/home-container.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import HomeOffers from "./components/home-offers/home-offers"
+import HomeSellers from "@/src/sections/home/components/home-sellers/components/seller-card"
 
 export default function HomeContainer() {
 	return (
 		<div>
 			<HomeOffers />
+			<HomeSellers />
 		</div>
 	)
 }
