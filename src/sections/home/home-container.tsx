6fac6594066437d2f0ffc50977104a142e30359a
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-12 13:01:40 -0400

    refactor(home): created home-promotions-carousel component for replace home-offers and home-sellers

--- src/sections/home/home-container.tsx
+++ src/sections/home/home-container.tsx
@@ -1,14 +1,88 @@
 import React from "react"
-import HomeOffers from "./components/home-offers/home-offers"
-import HomeSellers from "@/src/sections/home/components/home-sellers/components/seller-card"
 import HomeBanner from "@/src/sections/home/components/home-banner/home-banner"
+import HomePromotionsCarousel from "@/src/sections/home/components/home-promotions-carousel/home-promotions-carousel"
+
+const data = [
+	{
+		id: "1",
+		brand: "Christian Dior",
+		name: "Sauvage",
+		description:
+			"Una de las mejores fragancias que podrás encontrar sin duda alguna",
+		price: 90,
+		image: "/images/place-holder.jpg",
+	},
+	{
+		id: "2",
+		brand: "Dior",
+		name: "Sauvage",
+		description:
+			"Una de las mejores fragancias que podrás encontrar sin duda alguna",
+		price: 90,
+		image: "/images/place-holder.jpg",
+	},
+	{
+		id: "3",
+		brand: "Christian Dior",
+		name: "Sauvage",
+		description:
+			"Una de las mejores fragancias que podrás encontrar sin duda alguna",
+		price: 90,
+		image: "/images/place-holder.jpg",
+	},
+	{
+		id: "4",
+		brand: "Christian Dior",
+		name: "Sauvage",
+		description:
+			"Una de las mejores fragancias que podrás encontrar sin duda alguna",
+		price: 90,
+		image: "/images/place-holder.jpg",
+	},
+	{
+		id: "5",
+		brand: "Christian Dior",
+		name: "Sauvage",
+		description:
+			"Una de las mejores fragancias que podrás encontrar sin duda alguna",
+		price: 90,
+		image: "/images/place-holder.jpg",
+	},
+	{
+		id: "6",
+		brand: "Christian Dior",
+		name: "Sauvage",
+		description:
+			"Una de las mejores fragancias que podrás encontrar sin duda alguna",
+		price: 90,
+		image: "/images/place-holder.jpg",
+	},
+	{
+		id: "7",
+		brand: "Christian Dior",
+		name: "Sauvage",
+		description:
+			"Una de las mejores fragancias que podrás encontrar sin duda alguna",
+		price: 90,
+		image: "/images/place-holder.jpg",
+	},
+	{
+		id: "8",
+		brand: "Christian Dior",
+		name: "Sauvage",
+		description:
+			"Una de las mejores fragancias que podrás encontrar sin duda alguna",
+		price: 90,
+		image: "/images/place-holder.jpg",
+	},
+]
 
 export default function HomeContainer() {
 	return (
 		<div className="w-full flex flex-col gap-4">
 			<HomeBanner />
-			<HomeOffers />
-			<HomeSellers />
+			<HomePromotionsCarousel title="Perfumes en Oferta" data={data} />
+			<HomePromotionsCarousel title="Perfumes más Vendidos" data={data} />
 		</div>
 	)
 }
