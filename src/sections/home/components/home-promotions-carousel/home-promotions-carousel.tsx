ed48247a8811ea8ada1d83aca560b8a536166516
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-12 13:01:40 -0400

    refactor(home): created home-promotions-carousel component for replace home-offers and home-sellers

---
+++ src/sections/home/components/home-promotions-carousel/home-promotions-carousel.tsx
@@ -0,0 +1,27 @@
+"use client"
+import PerfurmCard, { Perfum } from "@/src/components/perfum-card/perfum-card"
+import { StandardCarouselProvider } from "@/src/components/ui/standard-carousel/context/standard-carousel-context"
+import StandardCarousel from "@/src/components/ui/standard-carousel/standard-carousel"
+import React from "react"
+
+interface Props {
+	title: string
+	data: Perfum[]
+}
+
+export default function HomePromotionsCarousel({ title, data }: Props) {
+	return (
+		<div className="flex flex-col gap-1">
+			<p className="text-2xl">{title}</p>
+			<StandardCarouselProvider>
+				<StandardCarousel
+					items={data}
+					dimension="100vw"
+					className="pl-4"
+					itemsStyles="basis-1/2 xs:basis-1/3 md:basis-1/4 lg:basis-1/4"
+					renderCard={item => <PerfurmCard perfum={item} />}
+				/>
+			</StandardCarouselProvider>
+		</div>
+	)
+}
