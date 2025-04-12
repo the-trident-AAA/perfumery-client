699a429efdfdc7be04d59a1ec0a442e9be7dcae9
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-11 23:26:01 -0400

    feat(app): added youtube-player component and used component in promotion section of footer

--- src/sections/root-layout/components/footer/footer.tsx
+++ src/sections/root-layout/components/footer/footer.tsx
@@ -3,12 +3,13 @@ import BrandSection from "@/src/sections/root-layout/components/footer/component
 import QuickLinks from "@/src/sections/root-layout/components/footer/components/quick-links/quick-links"
 import ContactInformation from "@/src/sections/root-layout/components/footer/components/contact-information/contact-information"
 import BottomSection from "@/src/sections/root-layout/components/footer/components/bottom-section/bottom-section"
+import Promotion from "@/src/sections/root-layout/components/footer/components/promotion/promotion"
 
 export default function Footer() {
 	return (
 		<footer className="bg-neutral-50 border-t">
 			<div className="container mx-auto px-4 py-12">
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+				<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8">
 					{/* Brand Section */}
 					<BrandSection />
 
@@ -17,6 +18,9 @@ export default function Footer() {
 
 					{/* Contact Information */}
 					<ContactInformation />
+
+					{/* Youtube Promotion */}
+					<Promotion />
 				</div>
 				{/* Bottom Section */}
 				<BottomSection />
