e88f7c70105dfd45b093192f8cad918c56f7b2a2
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-12 13:19:32 -0400

    feat(perfum-card): implemented responsive design for perfum-card

--- src/components/perfum-card/perfum-card.tsx
+++ src/components/perfum-card/perfum-card.tsx
@@ -3,12 +3,12 @@ import Image from "next/image"
 //import { Heart } from "lucide-react"
 
 export interface Perfum {
-  id: string;
-  brand: string;
-  name: string;
-  description: string;
-  price: number;
-  image: string;
+	id: string
+	brand: string
+	name: string
+	description: string
+	price: number
+	image: string
 }
 
 interface Props {
@@ -16,39 +16,45 @@ interface Props {
 }
 
 export default function PerfurmCard({
-  perfum: {id ,brand, name, description, price, image },
+	perfum: { id, brand, name, description, price, image },
 }: Props) {
 	return (
 		<div className="w-full max-w-xs bg-white rounded-lg shadow-sm overflow-hidden border border-gray-100">
 			<div className="relative">
 				<Image
-					src={image || "/placeholder.svg"}
+					className="aspect-square object-cover"
+					src={image}
 					alt={name}
-					width={300}
-					height={300}
-					className="w-full h-64 object-contain bg-gray-50 p-4"
+					width={400}
+					height={400}
 				/>
 				{/*<button className="absolute top-3 right-3 p-1" onClick={() => setIsFavorite(!isFavorite)}>
           <Heart className={`w-6 h-6 ${isFavorite ? "fill-black stroke-black" : "stroke-black"}`} />
         </button>*/}
 			</div>
 
-			<div className="p-4 space-y-2">
-				<h3 className="font-bold text-lg text-black">{brand}</h3>
+			<div className="p-4 sm:space-y-2">
+				<h3 className="font-bold text-base sm:text-lg text-black">
+					{brand}
+				</h3>
 				<div className="space-y-0.5">
 					<h4 className="text-sm font-medium text-blue-900">
 						{name}
 					</h4>
-					<p className="text-sm text-blue-900">{description}</p>
+					<p className="line-clamp-3 break-words text-xs text-secondary-app sm:text-sm">
+						{description}
+					</p>
 				</div>
 
-        <div className="pt-4 pb-2">
-          <p className="text-xl font-semibold">{price.toFixed(2)} $</p>
-        </div>
-      </div>
+				<div className="pt-1 sm:pt-4 sm:pb-2">
+					<p className="text-sm sm:text-xl font-semibold">
+						{price.toFixed(2)} $
+					</p>
+				</div>
+			</div>
 
 			<div className="border-t border-gray-200">
-				<button className="w-full py-3 text-center text-pink-600 font-medium hover:bg-pink-50 transition-colors">
+				<button className="w-full py-3 text-sm sm:text-xl text-center text-pink-600 font-medium hover:bg-pink-50 transition-colors">
 					Añadir al carrito
 				</button>
 			</div>
