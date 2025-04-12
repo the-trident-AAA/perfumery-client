d3b18f1e940a562c7eaef0a9697d63e5107c2134
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-12 14:49:12 -0400

    feat(root-layout): added modal-provider to root-layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,21 @@ import "../ui/globals.css"
 import { roboto } from "../ui/fonts"
 import Header from "../sections/root-layout/components/header/header"
 import Footer from "@/src/sections/root-layout/components/footer/footer"
+import { ModalProvider } from "@/src/components/modal/context/modalContext"
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<html>
 			<body className={`${roboto.className} antialiased`}>
-				<main className="flex min-h-screen flex-col">
-					<Header />
-					<div className="container mx-auto mt-5 px-3">
-						{children}
-					</div>
-					<Footer />
-				</main>
+				<ModalProvider>
+					<main className="flex min-h-screen flex-col">
+						<Header />
+						<div className="container mx-auto mt-5 px-3">
+							{children}
+						</div>
+						<Footer />
+					</main>
+				</ModalProvider>
 			</body>
 		</html>
 	)
