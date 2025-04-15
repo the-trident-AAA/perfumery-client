989bc05a5f53fe840da461dbee08219975ec28a9
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-15 15:12:23 -0400

    feat(header-navigation-menu): composed perfume navigation menu with perfume groups

--- src/sections/root-layout/components/header/components/header-navigation-menu/header-navigation-menu.tsx
+++ src/sections/root-layout/components/header/components/header-navigation-menu/header-navigation-menu.tsx
@@ -1,8 +1,10 @@
 import {
 	NavigationMenu,
+	NavigationMenuContent,
 	NavigationMenuItem,
 	NavigationMenuLink,
 	NavigationMenuList,
+	NavigationMenuTrigger,
 	navigationMenuTriggerStyle,
 } from "@/src/components/ui/navigation-menu"
 import { navigationItems } from "@/src/sections/root-layout/components/header/components/header-navigation-menu/lib/navigation-items"
@@ -13,24 +15,57 @@ export default function HeaderNavigationMenu() {
 	return (
 		<NavigationMenu className="mx-auto p-2">
 			<NavigationMenuList>
-				{navigationItems.map((navigationItem, index) => (
-					<NavigationMenuItem key={index}>
-						<NavigationMenuLink
-							className="text-[16px] sm:text-xl"
-							asChild
-						>
-							<Link
-								href={navigationItem.path}
-								className={navigationMenuTriggerStyle()}
+				{navigationItems.map((navigationItem, index) =>
+					!navigationItem.childrens ? (
+						<NavigationMenuItem key={index}>
+							<NavigationMenuLink
+								className="text-[16px] sm:text-xl"
+								asChild
 							>
+								<Link
+									href={navigationItem.path}
+									className={navigationMenuTriggerStyle()}
+								>
+									<div className="flex gap-2 items-center">
+										{navigationItem.icon &&
+											navigationItem.icon}
+										{navigationItem.label}
+									</div>
+								</Link>
+							</NavigationMenuLink>
+						</NavigationMenuItem>
+					) : (
+						<NavigationMenuItem key={index}>
+							<NavigationMenuTrigger className="text-[16px] sm:text-xl">
 								<div className="flex gap-2 items-center">
 									{navigationItem.icon && navigationItem.icon}
 									{navigationItem.label}
 								</div>
-							</Link>
-						</NavigationMenuLink>
-					</NavigationMenuItem>
-				))}
+							</NavigationMenuTrigger>
+							<NavigationMenuContent className="flex flex-col 2xl:flex-row gap-2">
+								{navigationItem.childrens.map(
+									(child, childIndex) => (
+										<NavigationMenuLink
+											className="text-[16px] sm:text-xl"
+											asChild
+											key={childIndex}
+										>
+											<Link
+												href={child.path}
+												className={navigationMenuTriggerStyle()}
+											>
+												<div className="flex gap-2 items-center">
+													{child.icon && child.icon}
+													{child.label}
+												</div>
+											</Link>
+										</NavigationMenuLink>
+									),
+								)}
+							</NavigationMenuContent>
+						</NavigationMenuItem>
+					),
+				)}
 			</NavigationMenuList>
 		</NavigationMenu>
 	)
