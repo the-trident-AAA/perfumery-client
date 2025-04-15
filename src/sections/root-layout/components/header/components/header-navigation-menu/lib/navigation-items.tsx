9d28176b65e04c23afde2dc52ca61233399811d7
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-15 15:12:23 -0400

    feat(header-navigation-menu): composed perfume navigation menu with perfume groups

--- src/sections/root-layout/components/header/components/header-navigation-menu/lib/navigation-items.tsx
+++ src/sections/root-layout/components/header/components/header-navigation-menu/lib/navigation-items.tsx
@@ -6,6 +6,7 @@ interface NavigationItem {
 	label: string
 	icon?: ReactNode
 	path: string
+	childrens?: NavigationItem[]
 }
 
 export const navigationItems: NavigationItem[] = [
@@ -18,5 +19,57 @@ export const navigationItems: NavigationItem[] = [
 		label: "Perfumes",
 		path: paths.perfumes().root,
 		icon: <ShoppingCartIcon color="black" className="size-4 sm:size-6" />,
+		childrens: [
+			{
+				label: "Todos los Aromas",
+				path: paths.perfumes().root,
+				icon: (
+					<ShoppingCartIcon
+						color="black"
+						className="size-4 sm:size-6"
+					/>
+				),
+			},
+			{
+				label: "Diseñador",
+				path: paths.perfumes({ group: "diseñador" }).root,
+				icon: (
+					<ShoppingCartIcon
+						color="black"
+						className="size-4 sm:size-6"
+					/>
+				),
+			},
+			{
+				label: "Árabe",
+				path: paths.perfumes({ group: "arabe" }).root,
+				icon: (
+					<ShoppingCartIcon
+						color="black"
+						className="size-4 sm:size-6"
+					/>
+				),
+			},
+			{
+				label: "Nicho",
+				path: paths.perfumes({ group: "nicho" }).root,
+				icon: (
+					<ShoppingCartIcon
+						color="black"
+						className="size-4 sm:size-6"
+					/>
+				),
+			},
+			{
+				label: "Nacional",
+				path: paths.perfumes({ group: "nacional" }).root,
+				icon: (
+					<ShoppingCartIcon
+						color="black"
+						className="size-4 sm:size-6"
+					/>
+				),
+			},
+		],
 	},
 ]
