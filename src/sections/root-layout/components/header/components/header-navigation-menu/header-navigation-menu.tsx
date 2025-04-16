728d8f41d29baeb682a2f39ed3ab16690901230b
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-15 22:00:52 -0400

    feat(header-navigation-menu): marked menu item perfumes as link-item

--- src/sections/root-layout/components/header/components/header-navigation-menu/header-navigation-menu.tsx
+++ src/sections/root-layout/components/header/components/header-navigation-menu/header-navigation-menu.tsx
@@ -9,7 +9,24 @@ import {
 } from "@/src/components/ui/navigation-menu"
 import { navigationItems } from "@/src/sections/root-layout/components/header/components/header-navigation-menu/lib/navigation-items"
 import Link from "next/link"
-import React from "react"
+import React, { ReactNode } from "react"
+
+interface LinkItemProps {
+	label: string
+	icon?: ReactNode
+	path: string
+}
+
+const LinkItem = ({ label, icon, path }: LinkItemProps) => (
+	<NavigationMenuLink className="text-[16px] sm:text-xl" asChild>
+		<Link href={path} className={navigationMenuTriggerStyle()}>
+			<div className="flex gap-2 items-center">
+				{icon && icon}
+				{label}
+			</div>
+		</Link>
+	</NavigationMenuLink>
+)
 
 export default function HeaderNavigationMenu() {
 	return (
@@ -18,29 +35,20 @@ export default function HeaderNavigationMenu() {
 				{navigationItems.map((navigationItem, index) =>
 					!navigationItem.childrens ? (
 						<NavigationMenuItem key={index}>
-							<NavigationMenuLink
-								className="text-[16px] sm:text-xl"
-								asChild
-							>
-								<Link
-									href={navigationItem.path}
-									className={navigationMenuTriggerStyle()}
-								>
-									<div className="flex gap-2 items-center">
-										{navigationItem.icon &&
-											navigationItem.icon}
-										{navigationItem.label}
-									</div>
-								</Link>
-							</NavigationMenuLink>
+							<LinkItem
+								label={navigationItem.label}
+								icon={navigationItem.icon}
+								path={navigationItem.path}
+							/>
 						</NavigationMenuItem>
 					) : (
 						<NavigationMenuItem key={index}>
 							<NavigationMenuTrigger className="text-[16px] sm:text-xl">
-								<div className="flex gap-2 items-center">
-									{navigationItem.icon && navigationItem.icon}
-									{navigationItem.label}
-								</div>
+								<LinkItem
+									label={navigationItem.label}
+									icon={navigationItem.icon}
+									path={navigationItem.path}
+								/>
 							</NavigationMenuTrigger>
 							<NavigationMenuContent className="flex flex-col 2xl:flex-row gap-2">
 								{navigationItem.childrens.map(
