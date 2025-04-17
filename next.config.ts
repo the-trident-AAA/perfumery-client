de7b18cd9578f174f45d40acd7499d3d40aa5a54
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-16 23:56:58 -0400

    chore(next.config): defined experimental server actions

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: "standalone",
+	experimental: {
+		serverActions: {
+			bodySizeLimit: "2mb",
+		},
+	},
 }
 
 export default nextConfig
