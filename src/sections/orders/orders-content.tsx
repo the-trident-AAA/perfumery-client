798dee6f15004ffd96615d2c8ef32616fc233315
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-04-18 23:27:48 -0400

    feat(orders-content): changed title of orders-content

--- src/sections/orders/orders-content.tsx
+++ src/sections/orders/orders-content.tsx
@@ -1,16 +1,14 @@
 import OrdersList from "@/src/sections/orders/orders-list/orders-list"
+import { ListOrderedIcon } from "lucide-react"
 import React from "react"
 
 export default function OrdersContent() {
 	return (
-		<div className="flex flex-col gap-2">
-			<div className="flex flex-col gap-2">
-				<h1 className="text-2xl font-bold">Pedidos</h1>
-				<p className="text-sm sm:text-lg text-gray-500">
-					Aquí puedes ver todos tus pedidos.
-				</p>
+		<div className="flex flex-col gap-2 mt-2">
+			<div className="flex  justify-center gap-2">
+				<ListOrderedIcon className="size-8" />
+				<h1 className="text-2xl font-bold">Pedidos Realizados</h1>
 			</div>
-
 			<OrdersList />
 		</div>
 	)
