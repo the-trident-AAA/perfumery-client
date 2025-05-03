0e5e9d9d33fa9e192d2dda0b0086b61283b6d6aa
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-03 13:07:03 -0400

    feat(orders): implemented perfumes orders list

---
+++ src/sections/orders/perfume-orders/perfume-order-card/perfume-order-card.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import Image from "next/image"
+import { Perfum } from "@/src/components/perfum-card/perfum-card"
+import QuantityController from "@/src/components/quantity-controller/quantity-controller"
+
+export interface PerfumeOrder {
+	id: string
+	perfume: Perfum
+	cant: number
+	precio: number
+}
+
+interface PerfumeCartProps {
+	perfumeOrder: PerfumeOrder
+}
+
+export default function PerfumeOrderCard({
+	perfumeOrder: { id, perfume, cant, precio },
+}: PerfumeCartProps) {
+	return (
+		<div className=" rounded-2xl border">
+			<div className="p-0">
+				<div className={`flex items-center`}>
+					{/* Imagen del producto */}
+					<div
+						className={`relative h-28 w-28 2xs:h-32 2xs:w-32 flex-shrink-0`}
+					>
+						<Image
+							className="aspect-square object-cover"
+							src={perfume.image}
+							alt={"image"}
+							width={400}
+							height={400}
+						/>
+					</div>
+
+					{/* Información del producto */}
+					<div className="flex flex-1 flex-col p-4">
+						<div className="flex flex-col sm:flex-row sm:justify-between">
+							<h3 className="font-medium text-base sm:text-lg truncate">
+								{perfume.name}
+							</h3>
+							<p className="font-semibold text-sm 2xs:text-base">
+								${precio.toFixed(2)}
+							</p>
+						</div>
+
+						<p className="text-sm text-muted-foreground line-clamp-2 mt-1">
+							{perfume.description}
+						</p>
+
+						{/* Controles de cantidad */}
+						<div className="flex items-center flex-row gap-1 justify-between mt-3">
+							<div className="flex flex-col 2xs:flex-row gap-1 items-center">
+								<p className="font-medium text-sm 2xs:text-base ">
+									Cantidad:{" "}
+								</p>
+								<span className="font-bold text-xs 2xs:text-base">
+									{cant}
+								</span>
+							</div>
+							<div className="flex flex-col items-center 2xs:flex-row gap-1">
+								<p className="font-medium text-sm 2xs:text-base ">
+									Total:{" "}
+								</p>
+								<span className="font-bold text-xs 2xs:text-base">
+									${(precio * cant).toFixed(2)}
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
