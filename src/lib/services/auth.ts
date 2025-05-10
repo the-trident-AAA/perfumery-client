bda5c8d1897e5bbe5d2836ca7e98063ff1a9ab1e
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-10 10:29:28 -0400

    chore: changed env api-url-server name

--- src/lib/services/auth.ts
+++ src/lib/services/auth.ts
@@ -1,11 +1,11 @@
 "use server"
 
-import { API_URL } from "@/src/config/env-server"
+import { NEXT_PUBLIC_API_URL } from "@/src/config/env-server"
 import { Credentials, credentialsSchema } from "@/src/lib/types/auth"
 
 export async function login(data: Credentials): Promise<any> {
 	const credentials = credentialsSchema.parse(data)
-	const res = await fetch(API_URL + "/auth/sign-in", {
+	const res = await fetch(NEXT_PUBLIC_API_URL + "/auth/sign-in", {
 		method: "POST",
 		body: JSON.stringify(credentials),
 	})
