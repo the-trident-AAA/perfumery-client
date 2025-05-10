b38c403153c81abac848f7122e6d39aaf2ba23ed
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-10 10:29:28 -0400

    chore: changed env api-url-server name

--- src/config/env-server.ts
+++ src/config/env-server.ts
@@ -1,8 +1,8 @@
 import { z } from "zod"
 
-export const { API_URL, AUTH_SECRET } = z
+export const { NEXT_PUBLIC_API_URL, AUTH_SECRET } = z
 	.object({
-		API_URL: z.string().url(),
+		NEXT_PUBLIC_API_URL: z.string().url(),
 		AUTH_SECRET: z.string().min(32),
 	})
 	.parse(process.env)
