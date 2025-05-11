c2c03b935bd4db8a3457834fc012a025e027bdd8
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-11 00:10:31 -0400

    feat(perfumes-list): added empty-content to perfumes fetching

--- src/sections/perfumes/list/perfumes-list.tsx
+++ src/sections/perfumes/list/perfumes-list.tsx
@@ -1,3 +1,4 @@
+import EmptyContent from "@/src/components/empty-content/empty-content"
 import PerfurmCard from "@/src/components/perfum-card/perfum-card"
 import { getPerfumesList } from "@/src/lib/services/perfumes"
 import React from "react"
@@ -8,11 +9,16 @@ export default async function PerfumesList() {
 	if (!res.response || res.error) throw new Error("Error fetching perfumes")
 
 	const perfumes = res.response
-	return (
+	return perfumes.length > 0 ? (
 		<div className="grid grid-cols-2 2xs:grid-cols-3 xl:grid-cols-4 gap-6">
 			{perfumes.map(perfume => (
 				<PerfurmCard key={perfume.id} perfume={perfume} />
 			))}
 		</div>
+	) : (
+		<EmptyContent
+			title="No hay perfumes disponibles"
+			description="De momento la tienda no cuenta con perfumes para ofrecer"
+		/>
 	)
 }
