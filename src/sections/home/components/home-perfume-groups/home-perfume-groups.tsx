d82fc5649f670630ca77a42497a892ee6748c163
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-14 14:58:35 -0400

    fix: adjusted cards-skeleton-groups in the home-sections

--- src/sections/home/components/home-perfume-groups/home-perfume-groups.tsx
+++ src/sections/home/components/home-perfume-groups/home-perfume-groups.tsx
@@ -10,7 +10,7 @@ export default function HomePerfumeGroups() {
 				fallback={
 					<CardSkeletonGroup
 						containerClassName="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full"
-						count={3}
+						count={4}
 					/>
 				}
 			>
