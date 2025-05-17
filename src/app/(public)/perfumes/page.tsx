1aed3c985966f0d64e1e9d92d9f87626be8fa985
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-17 15:52:35 -0400

    fix(page): update revalidation strategy for Home and Perfumes pages

--- src/app/(public)/perfumes/page.tsx
+++ src/app/(public)/perfumes/page.tsx
@@ -1,6 +1,8 @@
 import PerfumesContainer from "@/src/sections/perfumes/perfumes-container"
 import React from "react"
 
+export const revalidate = 60
+
 export default function PerfumesPage() {
 	return <PerfumesContainer />
 }
