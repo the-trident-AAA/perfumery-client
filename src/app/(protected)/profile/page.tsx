036b3b91d9a641c088563e84bbc38d9e199413ff
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-17 16:03:30 -0400

    feat(profile-page): enable dynamic rendering for ProfilePage component

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -1,6 +1,8 @@
 import ProfileContainer from "@/src/sections/profile/profile-container"
 import React from "react"
 
+export const dynamic = "force-dynamic"
+
 export default function ProfilePage() {
 	return <ProfileContainer />
 }
