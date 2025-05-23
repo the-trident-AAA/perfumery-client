c71cb9b98b3611b2814edf6cec90bc174fa8a53c
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-23 16:26:26 -0400

    feat(auth): implement sign-in path redirection in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,13 @@
-export { auth as middleware } from "@/src/auth"
+import { auth } from "@/src/auth"
+import { paths } from "@/src/lib/routes/paths"
+
+export default auth(req => {
+	if (!req.auth && req.nextUrl.pathname !== paths.sign_in.root) {
+		const newUrl = new URL(paths.sign_in.root, req.nextUrl.origin)
+		return Response.redirect(newUrl)
+	}
+})
+
+export const config = {
+	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
