7050fbc498a7e7ef7434a7dd880fd20aac1a31e1
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-24 12:01:29 -0400

    fix(user): remove click handler from user button for cleaner UI

--- src/sections/root-layout/components/header/components/user.tsx
+++ src/sections/root-layout/components/header/components/user.tsx
@@ -18,7 +18,6 @@ const User = () => {
 			<PopoverTrigger asChild>
 				<button
 					className="flex items-center justify-center rounded-full bg-transparent hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-					onClick={() => console.log("User button clicked")}
 					aria-label="User menu"
 				>
 					<UserCircleIcon className="size-8 sm:size-12 text-white" />
