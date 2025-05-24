8cd7867a5a1d9d2a8a6b2c7146a2f09b62f81e4b
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-24 11:40:06 -0400

    fix(user): remove LoginButton component and integrate sign-in button directly in user menu

--- src/sections/root-layout/components/header/components/user.tsx
+++ src/sections/root-layout/components/header/components/user.tsx
@@ -7,10 +7,8 @@ import {
 	PopoverTrigger,
 } from "@/src/components/ui/popover"
 import { paths } from "@/src/lib/routes/paths"
-import LoginButton from "@/src/sections/root-layout/components/header/components/login-button/login-button"
 import RegistrationButton from "@/src/sections/root-layout/components/header/components/registration-button/registration-button"
 import { UserCircleIcon } from "lucide-react"
-import Link from "next/link"
 import React from "react"
 
 const User = () => {
@@ -27,7 +25,9 @@ const User = () => {
 			</PopoverTrigger>
 			<PopoverContent className="w-full">
 				<div className="flex flex-col gap-1">
-					<LoginButton />
+					<NavigationComponent href={paths.sign_in.root}>
+						<Button className="w-full">Iniciar Sesión</Button>
+					</NavigationComponent>
 					<RegistrationButton />
 					<NavigationComponent href={paths.profile.root}>
 						<Button className="w-full">Ver Pérfil</Button>
