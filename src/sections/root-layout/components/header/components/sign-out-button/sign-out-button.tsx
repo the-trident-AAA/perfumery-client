b9c25f7c3ae2604cb8570fbdb29501dbc8cf8300
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-25 15:48:05 -0400

    fix(sign-out): add route protection check before redirecting on sign out

--- src/sections/root-layout/components/header/components/sign-out-button/sign-out-button.tsx
+++ src/sections/root-layout/components/header/components/sign-out-button/sign-out-button.tsx
@@ -1,15 +1,16 @@
 "use client"
 import { Button } from "@/src/components/ui/button"
-import { paths } from "@/src/lib/routes/paths"
+import { isProtectedRoute, paths } from "@/src/lib/routes/paths"
 import useSignOut from "@/src/sections/sign-in/form/hooks/use-sign-out"
-import { useRouter } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 import React, { useCallback } from "react"
 
 export default function SignOutButton() {
 	const router = useRouter()
+	const currentPath = usePathname()
 	const { signOut, loading } = useSignOut({
 		onSignOutAction: () => {
-			router.push(paths.sign_in.root)
+			if (isProtectedRoute(currentPath)) router.push(paths.sign_in.root)
 		},
 	})
 
