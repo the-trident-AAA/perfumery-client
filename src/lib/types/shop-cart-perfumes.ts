ab29cff850c800ff831a3b3d394331028bc82505
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-26 23:18:19 -0400

    feat(shop-cart): add shop cart routes and types for handling shop cart data

---
+++ src/lib/types/shop-cart-perfumes.ts
@@ -0,0 +1,7 @@
+import { PerfumeDetails } from "./perfumes"
+
+export interface ShopCartPerfume {
+	id: string
+	perfume: PerfumeDetails
+	cant: number
+}
---
+++ src/lib/types/shop-cart.ts
@@ -0,0 +1,6 @@
+import { ShopCartPerfume } from "@/src/lib/types/shop-cart-perfumes"
+
+export interface ShopCart {
+	id: string
+	shopCartPerfumes: ShopCartPerfume[]
+}
