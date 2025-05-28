620a4d9e4af7cef1c4f3319631f47a150086db48
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-28 12:02:44 -0400

    feat(shop-cart): implement clearShopCartModalContainer with confirmation panel

---
+++ src/sections/shop-cart/clear-shop-cart/clear-shop-cart-modal-container.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import ConfirmationPanel from "@/src/components/confirmation-panel/confirmation-panel"
+import { ModalContext } from "@/src/components/modal/context/modalContext"
+import { modalTypes } from "@/src/components/modal/types/modalTypes"
+import { ShopCartTotalItemsContext } from "@/src/sections/shop-cart/context/shop-cart-total-items-context"
+import useClearShopCart from "@/src/sections/shop-cart/hooks/use-clear-shop-cart"
+import { useCallback, useContext } from "react"
+import { toast } from "react-toastify"
+
+export default function clearShopCartModalContainer() {
+	const { getInfoModal, handleCloseModal } = useContext(ModalContext)
+	const { fetchShopCartTotalItems } = useContext(ShopCartTotalItemsContext)
+	const infoModal = getInfoModal(modalTypes.clearShopCartModal.name)
+	const id = infoModal && infoModal.entity ? infoModal.entity : null
+	const actionExecute = infoModal && infoModal.actionExecute
+	const { clearShopCart, loading } = useClearShopCart({
+		shopCartId: id,
+		onClearAction: () => {
+			toast.success("Carrito de Compras limpiado con éxito", {
+				position: "bottom-center",
+			})
+			if (actionExecute) actionExecute()
+			fetchShopCartTotalItems()
+		},
+	})
+	const onCloseModal = useCallback(() => {
+		handleCloseModal(modalTypes.clearShopCartModal.name)
+	}, [handleCloseModal])
+	return (
+		<ConfirmationPanel
+			title={
+				modalTypes.clearShopCartModal.title || "Elminación de la Marca"
+			}
+			message={modalTypes.clearShopCartModal.message}
+			warningMessage={modalTypes.clearShopCartModal.warningMessage}
+			onConfirm={clearShopCart}
+			onCancel={onCloseModal}
+			isLoading={loading}
+			isDestructive
+		/>
+	)
+}
