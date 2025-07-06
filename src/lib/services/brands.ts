046ecbb05a58764e348f77809184fd6200492a83
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-06 12:09:16 -0400

    feat(brands): add getBrandsList function to get the list of brands from the API

---
+++ src/lib/services/brands.ts
@@ -0,0 +1,21 @@
+"use server"
+import { buildApiResponse } from "@/src/lib/api"
+import { QueryParamsURLFactory } from "@/src/lib/request"
+import {
+	apiRoutes,
+	tagsCacheByRoutes,
+} from "@/src/lib/routes/api-routes/api-routes"
+import { Brand } from "@/src/lib/types/brands"
+import { Offer } from "@/src/lib/types/offers"
+import { IQueryable } from "@/src/lib/types/request"
+
+export async function getBrandsList(params: IQueryable) {
+	const url = new QueryParamsURLFactory(params, apiRoutes.brands.get).build()
+
+	const res = await fetch(url, {
+		method: "GET",
+		next: { tags: [tagsCacheByRoutes.brands.multipleTag] },
+	})
+
+	return await buildApiResponse<Brand[]>(res)
+}
