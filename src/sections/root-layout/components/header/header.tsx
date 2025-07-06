ceb358b6514d2de184d604263e8c9049cc882dd7
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-06 13:22:17 -0400

    refactor(header): remove text color class in the Header component to improve readability

--- src/sections/root-layout/components/header/header.tsx
+++ src/sections/root-layout/components/header/header.tsx
@@ -6,7 +6,7 @@ import HeaderNavigationOptions from "@/src/sections/root-layout/components/heade
 const Header = () => {
 	return (
 		<header className="bg-primary sticky top-0 z-50 shadow-md flex flex-col">
-			<div className="text-white container mx-auto py-4 px-6">
+			<div className="container mx-auto py-4 px-6">
 				<div className="flex items-center justify-between h-16">
 					{/* Logo - alineado a la izquierda */}
 					<div className="flex-shrink-0 flex items-center">
