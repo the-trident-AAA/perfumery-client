a54d895b2d1f07c11f8537a4620838d216c1472b
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-06 12:06:36 -0400

    feat(brands): add BrandFiltersDTO and convertBrandFiltersDTO functions to handle brand filters

--- src/lib/types/brands.ts
+++ src/lib/types/brands.ts
@@ -1,4 +1,16 @@
+import { BrandsFilters } from "@/src/sections/brands/filters/hooks/use-brands-filters"
+
 export interface Brand {
 	id: string
 	name: string
+}
+
+export interface BrandFiltersDTO {
+	name?: string
+}
+
+export const convertBrandFiltersDTO = (
+	brandFilters: BrandsFilters,
+): BrandFiltersDTO => {
+	return { ...brandFilters }
 }
