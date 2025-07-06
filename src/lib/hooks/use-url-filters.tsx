fbd51b84c131ad96f39ab49c931b94cdda90cf2d
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-06 11:51:34 -0400

    feat(filters): add the useUrlFilters hook to handle URL filters efficiently

---
+++ src/lib/hooks/use-url-filters.tsx
@@ -0,0 +1,48 @@
+"use client"
+import { debounce } from "lodash"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { useCallback, useMemo } from "react"
+
+export default function useUrlFilters() {
+	const { replace } = useRouter()
+	const searchParams = useSearchParams()
+	const pathname = usePathname()
+
+	const debouncedUpdateFiltersInUrl = useMemo(
+		() =>
+			debounce((updatedFilters: object) => {
+				const searchUrl = new URLSearchParams(searchParams)
+				Object.entries(updatedFilters).forEach(([key, value]) => {
+					if (value !== undefined) {
+						if (typeof value === "number")
+							searchUrl.set(key, value.toString())
+						else if (typeof value === "boolean")
+							searchUrl.set(key, value ? "true" : "false")
+						else if (Array.isArray(value)) {
+							value.forEach(() => {
+								searchUrl.delete(key)
+							})
+							value.forEach(value => {
+								searchUrl.append(key, value)
+							})
+						} else searchUrl.set(key, value)
+					} else searchUrl.delete(key)
+				})
+				replace(
+					Object.keys(updatedFilters).length > 0
+						? `${pathname}?${searchUrl.toString()}`
+						: pathname,
+				)
+			}, 300),
+		[searchParams, pathname, replace],
+	)
+
+	const updateFiltersInUrl = useCallback(
+		(updatedFilters: object) => {
+			debouncedUpdateFiltersInUrl(updatedFilters)
+		},
+		[debouncedUpdateFiltersInUrl],
+	)
+
+	return { updateFiltersInUrl }
+}
