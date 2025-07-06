e377e10e246bcc81a60b226c12df769e7c7063b6
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-06 13:31:44 -0400

    feat(header-search): implement dynamic search in the HeaderSearch component with routing to the perfume page

--- src/sections/root-layout/components/header/components/header-search/header-search.tsx
+++ src/sections/root-layout/components/header/components/header-search/header-search.tsx
@@ -2,12 +2,30 @@
 import { Search as SearchIcon } from "lucide-react"
 import useHeaderSearch from "@/src/sections/root-layout/components/header/components/header-search/hooks/use-header-search"
 import SearchInput from "@/src/components/inputs/search-input/search-input"
+import { usePathname, useRouter } from "next/navigation"
+import { paths } from "@/src/lib/routes/paths"
+import { useState } from "react"
 
 const HeaderSearch = () => {
+	const [search, setSearch] = useState("")
+	const router = useRouter()
+	const currentPath = usePathname()
 	const { isMobile, toggleSearch, searchRef, showSearch } = useHeaderSearch()
 
 	return !isMobile ? (
-		<SearchInput id="name" />
+		<SearchInput
+			id="name"
+			value={search}
+			placeHolder="Comience a buscar perfumes..."
+			onChange={e => {
+				setSearch(e.target.value)
+				router.push(
+					paths.perfumes({
+						...(e.target.value && { name: e.target.value }),
+					}).root,
+				)
+			}}
+		/>
 	) : (
 		<div className="relative" ref={searchRef}>
 			<button
@@ -18,8 +36,22 @@ const HeaderSearch = () => {
 				<SearchIcon className="size-8 sm:size-12 text-white" />
 			</button>
 			{showSearch && (
-				<div className="absolute top-full right-[-50px] mt-2 w-64 bg-white shadow-lg rounded-lg p-2 z-10 border border-gray-200">
-					<SearchInput id="name" />
+				<div className="absolute top-full right-[-50px] mt-2 w-64 text-black bg-white shadow-lg rounded-lg p-2 z-10 border border-gray-200">
+					<SearchInput
+						id="name"
+						value={search}
+						placeHolder="Comience a buscar perfumes..."
+						onChange={e => {
+							setSearch(e.target.value)
+							router.push(
+								paths.perfumes({
+									...(e.target.value && {
+										name: e.target.value,
+									}),
+								}).root,
+							)
+						}}
+					/>
 				</div>
 			)}
 		</div>
