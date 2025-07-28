8854927410373b616364f48fabfdbf6953db2769
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-28 15:13:33 -0400

    feat(best-selling-products): update the design of the best-selling products section, improving the presentation with a new header and attractive description.

--- src/sections/home/components/best-selling-products/best-selling-products.tsx
+++ src/sections/home/components/best-selling-products/best-selling-products.tsx
@@ -4,18 +4,28 @@ import React, { Suspense } from "react"
 
 export default async function BestSellingProducts() {
 	return (
-		<div className="flex flex-col gap-1">
-			<p className="text-2xl">Productos más Vendidos</p>
-			<Suspense
-				fallback={
-					<CardSkeletonGroup
-						containerClassName="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full"
-						count={3}
-					/>
-				}
-			>
-				<BestSellingProductsList />
-			</Suspense>
-		</div>
+		<section className="bg-muted pt-12 pb-20 ">
+			<div className="container mx-auto flex flex-col gap-12 pt-4">
+				<div className="text-center space-y-4 ">
+					<h2 className="text-3xl lg:text-4xl font-bold text-primary">
+						Los favoritos de nuestros clientes
+					</h2>
+					<p className="text-xl text-black font-semibold max-w-2xl mx-auto">
+						Descubre los perfumes más vendidos y déjate cautivar por
+						las fragancias que todos aman
+					</p>
+				</div>
+				<Suspense
+					fallback={
+						<CardSkeletonGroup
+							containerClassName="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full"
+							count={3}
+						/>
+					}
+				>
+					<BestSellingProductsList />
+				</Suspense>
+			</div>
+		</section>
 	)
 }
