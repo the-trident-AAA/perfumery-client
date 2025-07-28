5b6810ec713ab6a9588aa7bf6dea3b41a1145b45
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-28 00:24:21 -0400

    feat(home-offers): update the design of the offers section, adding an attractive header and improving the presentation of the content

--- src/sections/home/components/home-offers/home-offers.tsx
+++ src/sections/home/components/home-offers/home-offers.tsx
@@ -4,18 +4,29 @@ import React, { Suspense } from "react"
 
 export default function HomeOffers() {
 	return (
-		<div className="flex flex-col gap-1">
-			<p className="text-2xl">Ofertas</p>
-			<Suspense
-				fallback={
-					<CardSkeletonGroup
-						containerClassName="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full"
-						count={3}
-					/>
-				}
-			>
-				<HomeOffersList />
-			</Suspense>
+		<div className="bg-muted pb-20">
+			<div className="container mx-auto flex flex-col gap-12 pt-4">
+				<div className="text-center space-y-4 ">
+					<h2 className="text-3xl lg:text-4xl font-bold text-primary">
+						Aromas irresistibles a precios especiales
+					</h2>
+					<p className="text-xl text-black font-semibold max-w-2xl mx-auto">
+						Aprovecha nuestras ofertas exclusivas en perfumes
+						seleccionados y encuentra tu fragancia ideal al mejor
+						precio
+					</p>
+				</div>
+				<Suspense
+					fallback={
+						<CardSkeletonGroup
+							containerClassName="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full"
+							count={3}
+						/>
+					}
+				>
+					<HomeOffersList />
+				</Suspense>
+			</div>
 		</div>
 	)
 }
