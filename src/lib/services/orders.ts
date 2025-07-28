8b33d7b80bacbdbb702e973407ff9c0d0b39630c
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-28 16:34:59 -0400

    feat(create-order): add the "content-type" header to the order creation request to ensure JSON formatting.

--- src/lib/services/orders.ts
+++ src/lib/services/orders.ts
@@ -6,10 +6,12 @@ import { Order } from "@/src/lib/types/orders"
 
 export async function createOrder() {
 	const session = await auth()
+
 	const res = await fetch(apiRoutes.orders.get, {
 		method: "POST",
 		headers: {
 			Authorization: "Bearer " + session?.accessToken,
+			"content-type": "application/json",
 		},
 	})
 	console.log(res)
