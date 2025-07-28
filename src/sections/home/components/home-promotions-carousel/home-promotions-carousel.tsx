f5cb8aa0857a98b869d7f846a0529fd761dfef4f
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-28 18:32:56 -0400

    feat(home-promotions-carousel): adjust element styles in the promotions carousel to improve presentation and code consistency

--- src/sections/home/components/home-promotions-carousel/home-promotions-carousel.tsx
+++ src/sections/home/components/home-promotions-carousel/home-promotions-carousel.tsx
@@ -18,7 +18,7 @@ export default function HomePromotionsCarousel({ data }: Props) {
 				items={data}
 				dimension="100vw"
 				className="pl-4 mr-4"
-				itemsStyles="sm:basis-1/2 md:basis-1/3 lg:basis-1/3 xl:basis-1/4 2xl:basis-1/5"
+				itemsStyles="sm:basis-1/2 md:basis-1/3 lg:basis-1/3 2xl:basis-1/4"
 				withArrows
 				arrowsPosition="outside"
 				arrowsLocation="bottom"
