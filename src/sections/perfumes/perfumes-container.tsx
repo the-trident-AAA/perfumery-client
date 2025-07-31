9ec5f74c4de4c0802f9060f71edaf75268711158
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-31 12:19:35 -0400

    feat(perfumes-container): integrate the DetailsSectionHeader component to improve the presentation of the perfume section.

--- src/sections/perfumes/perfumes-container.tsx
+++ src/sections/perfumes/perfumes-container.tsx
@@ -1,4 +1,5 @@
 import { CardSkeletonGroup } from "@/src/components/card-skeleton-group/card-skeleton-group"
+import DetailsSectionHeader from "@/src/components/details-section-header/details-section-header"
 import { SearchParamsPagination } from "@/src/lib/types/pagination"
 import PerfumesFiltersContainer from "@/src/sections/perfumes/filters/perfumes-filters-container/perfumes-filters-container"
 import PerfumesList from "@/src/sections/perfumes/list/perfumes-list"
@@ -12,10 +13,11 @@ export default function PerfumesContainer({ searchParams }: Props) {
 	return (
 		<div className="flex flex-col lg:flex-row justify-center p-12 gap-6">
 			<PerfumesFiltersContainer />
-			<div className="flex flex-col w-full gap-2">
-				<p className="text-lg sm:text-3xl font-semibold mb-6 text-gray-800">
-					Perfumes de la Tienda
-				</p>
+			<div className="flex flex-col w-full gap-8">
+				<DetailsSectionHeader
+					title="Perfumes de la Tienda"
+					description="Explore la enorme colección de perfumes de la tienda disponibles."
+				/>
 				<Suspense
 					fallback={
 						<CardSkeletonGroup
