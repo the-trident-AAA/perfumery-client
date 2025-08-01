274f6bc5b0356bb435401550516f5c779e8e9d16
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-07-31 22:44:55 -0400

    refactor(orders-content): improve the style of the placed orders header with updated background and colors for better visibility.

--- src/sections/orders/orders-content.tsx
+++ src/sections/orders/orders-content.tsx
@@ -5,9 +5,11 @@ import React from "react"
 export default function OrdersContent() {
 	return (
 		<div className="flex flex-col gap-2 mt-2">
-			<div className="flex  justify-center gap-2">
-				<ListOrderedIcon className="size-8" />
-				<h1 className="text-2xl font-bold">Pedidos Realizados</h1>
+			<div className="flex bg-primary p-2 rounded-2xl justify-center gap-2">
+				<ListOrderedIcon className="size-8 text-secondary" />
+				<h1 className="text-2xl text-secondary font-bold">
+					Pedidos Realizados
+				</h1>
 			</div>
 			<OrdersList />
 		</div>
