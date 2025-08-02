0cdd1707cddf6063aec1caf289972a46950843f2
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-02 16:02:44 -0400

    feat(orders): update the hook to get the user's total orders using the current session.

--- src/sections/orders/hooks/use-user-total-orders.tsx
+++ src/sections/orders/hooks/use-user-total-orders.tsx
@@ -1,27 +1,28 @@
 "use client"
 import { getUserTotalOrders } from "@/src/lib/services/orders"
+import { useSession } from "next-auth/react"
 import { useCallback, useEffect, useState } from "react"
 
-interface Props {
-	userId: string
-}
-
-export default function useUserTotalOrders({ userId }: Props) {
+export default function useUserTotalOrders() {
 	const [totalOrders, setTotalOrders] = useState(0)
 	const [loading, setLoading] = useState(false)
 	const [error, setError] = useState<string | null>(null)
-
+	const { data } = useSession()
 	const fetchUserTotalOrders = useCallback(async () => {
-		setLoading(true)
-		setError(null)
-		const res = await getUserTotalOrders(userId)
-		if (!res.response || res.error)
-			setError("Error en la obtención del total de pedidos del usuario")
-		else {
-			setTotalOrders(res.response.total)
+		if (data) {
+			setLoading(true)
+			setError(null)
+			const res = await getUserTotalOrders(data.user.id as string)
+			if (!res.response || res.error)
+				setError(
+					"Error en la obtención del total de pedidos del usuario",
+				)
+			else {
+				setTotalOrders(res.response.total)
+			}
+			setLoading(false)
 		}
-		setLoading(false)
-	}, [userId])
+	}, [data])
 
 	useEffect(() => {
 		fetchUserTotalOrders()
