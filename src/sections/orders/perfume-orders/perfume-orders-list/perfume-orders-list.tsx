3aff5bb7fea29a5a70291818954e83dae70cd83e
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-04 17:52:39 -0400

    feat(perfume-orders-list): implement refresh order data when changed form

--- src/sections/orders/perfume-orders/perfume-orders-list/perfume-orders-list.tsx
+++ src/sections/orders/perfume-orders/perfume-orders-list/perfume-orders-list.tsx
@@ -1,5 +1,7 @@
 "use client"
+import { Badge } from "@/src/components/ui/badge"
 import { OrderPerfume } from "@/src/lib/types/orders-perfumes"
+import { fCurrency } from "@/src/lib/utils/format-number"
 import { OrderPerfumeEdit } from "@/src/sections/orders/form/edit/schemas/order-perfume-schema"
 import PerfumeOrderCard from "@/src/sections/orders/perfume-orders/perfume-order-card/perfume-order-card"
 import { Sparkles } from "lucide-react"
@@ -17,36 +19,62 @@ export default function PerfumeOrdersList({
 	const { watch, setValue, trigger } = useFormContext()
 	const perfumes = watch("perfumes") as OrderPerfumeEdit[]
 
-	return perfumes.length > 0 ? (
-		<div
-			className={`grid ${variant === "default" ? "grid-cols-1 xl:grid-cols-2" : "grid-cols-1"} gap-4 pb-6`}
-		>
-			{perfumes.map((perfume, index) => {
-				const orderPerfume = perfumesOrder.find(
-					p => p.id === perfume.id,
-				)
-				return (
-					<PerfumeOrderCard
-						key={index}
-						index={index}
-						perfumeOrder={
-							{
-								...orderPerfume,
-								cant: perfume.cant,
-							} as OrderPerfume
-						}
-					/>
-				)
-			})}
-		</div>
-	) : (
-		<div className="text-center py-8">
-			<div className="bg-muted/50 rounded-full p-4 w-16 h-16 mx-auto mb-4 flex items-center justify-center">
-				<Sparkles className="h-8 w-8 text-muted-foreground" />
-			</div>
-			<p className="text-muted-foreground">
-				No hay perfumes en este pedido.
-			</p>
+	return (
+		<div className="space-y-4">
+			<Badge variant={"secondary"} className="text-primary text-base">
+				<p className="text-primary">
+					Costo del pedido actualizado:{" "}
+					{fCurrency(
+						perfumes.reduce((total, perfume) => {
+							const orderPerfume = perfumesOrder.find(
+								o => o.id === perfume.id,
+							)
+							return (
+								total +
+								(orderPerfume
+									? orderPerfume.perfume.price * perfume.cant
+									: 0)
+							)
+						}, 0),
+					)}{" "}
+				</p>
+			</Badge>
+
+			{perfumes.length > 0 ? (
+				<div
+					className={`grid ${variant === "default" ? "grid-cols-1 xl:grid-cols-2" : "grid-cols-1"} gap-4 pb-6`}
+				>
+					{perfumes.map((perfume, index) => {
+						const orderPerfume = perfumesOrder.find(
+							p => p.id === perfume.id,
+						)
+						return (
+							<PerfumeOrderCard
+								key={index}
+								index={index}
+								perfumeOrder={
+									{
+										...orderPerfume,
+										cant: perfume.cant,
+										price:
+											perfume.cant *
+											(orderPerfume?.perfume.price || 0),
+									} as OrderPerfume
+								}
+							/>
+						)
+					})}
+				</div>
+			) : (
+				<div className="text-center py-8">
+					<div className="bg-muted/50 rounded-full p-4 w-16 h-16 mx-auto mb-4 flex items-center justify-center">
+						<Sparkles className="h-8 w-8 text-muted-foreground" />
+					</div>
+					<p className="text-muted-foreground">
+						No hay perfumes en este pedido.
+					</p>
+				</div>
+			)}
 		</div>
 	)
 }
