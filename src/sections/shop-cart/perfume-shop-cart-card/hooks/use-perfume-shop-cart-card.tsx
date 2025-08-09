7de3067c509baffb837a6d8079aa29468aa1df75
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-09 11:59:00 -0400

    feat(use-perfume-shop-cart-card): add a call to fetchShopCartTotalItems in the edit action to update the total number of items in the cart.

--- src/sections/shop-cart/perfume-shop-cart-card/hooks/use-perfume-shop-cart-card.tsx
+++ src/sections/shop-cart/perfume-shop-cart-card/hooks/use-perfume-shop-cart-card.tsx
@@ -26,6 +26,7 @@ export default function usePerfumeShopCartCard({
 	} = useEditShopCartPerfume({
 		id: shopCartPerfume.id,
 		onEditAction: () => {
+			fetchShopCartTotalItems()
 			shopCartRefresh()
 		},
 	})
