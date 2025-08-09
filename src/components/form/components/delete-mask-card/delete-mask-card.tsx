604a32125374e2b7482088500aac22cdb7c39098
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-09 12:10:33 -0400

    feat(delete-mask-card): add a component to delete elements with a confirmation button, improving user interaction.

---
+++ src/components/form/components/delete-mask-card/delete-mask-card.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/src/components/ui/button"
+import { Trash2 } from "lucide-react"
+import React, { ReactNode } from "react"
+
+interface Props {
+	children: ReactNode
+	handleRemove: () => void
+}
+
+export default function DeleteMaskCard({ children, handleRemove }: Props) {
+	return (
+		<div className="relative group">
+			<div className="absolute inset-0 z-10 transition-all duration-200 rounded-lg bg-transparent group-hover:bg-black/5 flex items-center justify-center">
+				<Button
+					type="button"
+					className="absolute top-2 right-2 bg-red-500 text-white rounded-full p-1.5 opacity-0 group-hover:opacity-100 transition-opacity duration-200 hover:bg-red-600"
+					onClick={e => {
+						e.stopPropagation()
+						handleRemove()
+					}}
+					aria-label="Eliminar elemento"
+				>
+					<Trash2 className="h-4 w-4" />
+				</Button>
+			</div>
+			{children}
+		</div>
+	)
+}
