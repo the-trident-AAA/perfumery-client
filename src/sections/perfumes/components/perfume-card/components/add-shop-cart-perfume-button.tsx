beb0374f697bba2ab3477136a30b3dd6f23b3198
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-08 23:32:08 -0400

    feat(add-shop-cart-button): allow size variations on the add to cart button and adjust the accepted perfume type.

--- src/sections/perfumes/components/perfume-card/components/add-shop-cart-perfume-button.tsx
+++ src/sections/perfumes/components/perfume-card/components/add-shop-cart-perfume-button.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { Button } from "@/src/components/ui/button"
 import { paths } from "@/src/lib/routes/paths"
-import { Perfume } from "@/src/lib/types/perfumes"
+import { Perfume, PerfumeDetails } from "@/src/lib/types/perfumes"
 import { ShopCartContext } from "@/src/sections/shop-cart/context/shop-cart-context/shop-cart-context"
 import { ShopCartTotalItemsContext } from "@/src/sections/shop-cart/context/shop-cart-total-items-context/shop-cart-total-items-context"
 import useCreateShopCartPerfume from "@/src/sections/shop-cart/hooks/use-create-shop-cart-perfume"
@@ -12,10 +12,14 @@ import React, { useCallback, useContext, useEffect } from "react"
 import { toast } from "react-toastify"
 
 interface Props {
-	perfume: Perfume
+	perfume: Perfume | PerfumeDetails
+	variant?: "default" | "large"
 }
 
-export default function AddShopCartPerfumeButton({ perfume }: Props) {
+export default function AddShopCartPerfumeButton({
+	perfume,
+	variant = "default",
+}: Props) {
 	const { fetchShopCartTotalItems } = useContext(ShopCartTotalItemsContext)
 	const { fetchShopCart } = useContext(ShopCartContext)
 	const { createShopCartPerfume, loading, error } = useCreateShopCartPerfume({
@@ -56,11 +60,11 @@ export default function AddShopCartPerfumeButton({ perfume }: Props) {
 
 	return (
 		<Button
+			size={variant === "large" ? "lg" : "default"}
+			variant={variant === "large" ? "secondary" : "default"}
 			className={`${
-				perfume.available
-					? `bg-gradient-to-br from-primary via-primary/90 to-primary/80 hover:shadow-lg transform hover:scale-105`
-					: "bg-gradient-to-br from-primary via-primary/90 to-primary/80"
-			} transition-all duration-300 text-secondary font-bold px-6`}
+				variant === "large" ? "text-primary flex-1" : "text-secondary"
+			} transition-all duration-300 px-6`}
 			disabled={loading || !perfume.available}
 			onClick={handleAddShopCartPerfume}
 		>
