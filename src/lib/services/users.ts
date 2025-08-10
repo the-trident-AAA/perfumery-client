84f7448d3dc62ff7a82b2d71f8c9c47e50b899bf
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-10 11:20:24 -0400

    feat(users-service): implement a function to obtain a user by ID, improving user management in the service.

---
+++ src/lib/services/users.ts
@@ -0,0 +1,22 @@
+"use server"
+
+import { auth } from "@/src/auth"
+import { buildApiResponse } from "@/src/lib/api"
+import {
+	apiRoutes,
+	tagsCacheByRoutes,
+} from "@/src/lib/routes/api-routes/api-routes"
+import { User } from "@/src/lib/types/users"
+
+export async function getUserById(id: string) {
+	const session = await auth()
+	const res = await fetch(apiRoutes.users.getById.replace(":id", id), {
+		method: "GET",
+		headers: {
+			Authorization: "Bearer " + session?.accessToken,
+		},
+		next: { tags: [tagsCacheByRoutes.users.singleTag + ": " + id] },
+	})
+
+	return await buildApiResponse<User>(res)
+}
