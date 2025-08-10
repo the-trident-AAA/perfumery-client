f76c610d56f8359fc2ad93bda660d9a234b48874
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-10 13:15:46 -0400

    feat(user-edit): add UserEditDTO interface and function to convert user edit data, improving user management.

--- src/lib/types/users.ts
+++ src/lib/types/users.ts
@@ -1,7 +1,21 @@
+import { UserEdit } from "@/src/sections/users/form/edit/schemas/edit-user-schema"
+
 export interface User {
 	id: string
 	username: string
 	avatar?: string
 	email: string
 	role: string
+}
+
+export interface UserEditDTO {
+	username: string
+}
+
+export const convertUserEditDTO = (
+	userEdit: Omit<UserEdit, "avatar">,
+): UserEditDTO => {
+	return {
+		...userEdit,
+	}
 }
