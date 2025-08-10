ca79ed25d606870f16bb98523adfd9e034fe4085
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-10 16:09:50 -0400

    feat(auth): add interface and function to handle password change, improving authentication management.

--- src/lib/types/auth.ts
+++ src/lib/types/auth.ts
@@ -1,8 +1,14 @@
 import { Credentials } from "@/src/sections/sign-in/form/schemas/credentials-schema"
+import { ChangePassword } from "@/src/sections/users/form/profile/components/change-password-mode-form/schemas/change-password-schema"
 
 export interface CredentialsDTO {
 	username: string
 	password: string
+}
+
+export interface ChangePasswordDTO {
+	currentPassword: string
+	newPassword: string
 }
 
 export const convertCredentialsDTO = (
@@ -11,5 +17,13 @@ export const convertCredentialsDTO = (
 	return {
 		...credentials,
 		username: credentials.firstCredential,
+	}
+}
+
+export const convertChangePasswordDTO = (
+	changePassword: Omit<ChangePassword, "confirmPassword">,
+): ChangePasswordDTO => {
+	return {
+		...changePassword,
 	}
 }
