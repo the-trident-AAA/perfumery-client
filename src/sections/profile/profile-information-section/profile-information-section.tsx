994ccf0e5d261532c90521b088da116c6ad38df8
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-10 22:39:35 -0400

    feat(profile): integrate user profile data upload, updating avatar and user information in the profile section.

--- src/sections/profile/profile-information-section/profile-information-section.tsx
+++ src/sections/profile/profile-information-section/profile-information-section.tsx
@@ -1,23 +1,28 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/src/components/ui/avatar"
-import { Button } from "@/src/components/ui/button"
+import { getUserProfile } from "@/src/lib/services/users"
+
 import EditProfileButton from "@/src/sections/profile/profile-information-section/edit-profile-button/edit-profile-button"
 
-interface Props {}
+export default async function ProfileInformationSection() {
+	const res = await getUserProfile()
 
-export default function ProfileInformationSection() {
+	if (!res.response || res.error)
+		throw new Error("Error en la carga del perfil")
+
+	const user = res.response
 	return (
 		<div className="flex flex-col items-center p-6 max-w-xs">
 			<Avatar className="size-56">
-				<AvatarImage src="/images/place-holder.jpg" />
-				<AvatarFallback>AP</AvatarFallback>
+				<AvatarImage src={user.avatar} />
+				<AvatarFallback>{user.username.charAt(0)}</AvatarFallback>
 			</Avatar>
 
 			<h1 className="text-2xl font-bold text-secondary mt-2">
-				Andy Pelaez
+				{user.username}
 			</h1>
 
 			<p className="text-secondary font-semibold text-sm mt-1 mb-6">
-				andyP02@gmail.com
+				{user.email}
 			</p>
 
 			<EditProfileButton />
