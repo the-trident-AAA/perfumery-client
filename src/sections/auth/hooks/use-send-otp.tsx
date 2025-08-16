f7bdbfa942eca5b7a2de4ad2b3f2ee42a27e54e7
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-16 13:18:10 -0400

    feat(auth): implement hooks to send OTPs and handle loading status and errors

---
+++ src/sections/auth/hooks/use-send-otp.tsx
@@ -0,0 +1,39 @@
+"use client"
+import { sendOtp as sendOtpService } from "@/src/lib/services/auth"
+import { useCallback, useState } from "react"
+
+interface Props {
+	id: string
+	onSendAction: () => void
+}
+
+export default function useEditUser({ id, onSendAction }: Props) {
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState<string | null>(null)
+
+	const sendOtp = useCallback(async () => {
+		try {
+			setLoading(true)
+			setError(null)
+
+			const res = await sendOtpService(id)
+			if (!res.response || res.error)
+				setError(
+					res.error?.reason ||
+						"Error en el envío del código de verificación",
+				)
+			else {
+				onSendAction()
+			}
+		} catch (error) {
+			console.log(error)
+		} finally {
+			setLoading(false)
+		}
+	}, [onSendAction, id])
+	return {
+		loading,
+		error,
+		sendOtp,
+	}
+}
