1a901c1f3bfd4645300a4744c1dcdfa16e4cd044
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-16 12:13:38 -0400

    feat(modal): change the background color of the modal to a softer tone to improve visual aesthetics.

--- src/app/@modal/layout.tsx
+++ src/app/@modal/layout.tsx
@@ -9,7 +9,7 @@ export default function ModalLayout({
 }) {
 	return (
 		<div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-			<div className="relative flex flex-col gap-4 items-center justify-center bg-white p-4 rounded-2xl shadow-xl max-w-2xl w-full">
+			<div className="relative flex flex-col gap-4 items-center justify-center bg-muted p-4 rounded-2xl shadow-xl max-w-2xl w-full">
 				{/* Botón de cerrar */}
 				<div className="flex w-full gap-2 justify-between items-center">
 					<p>Title</p>
