2047632588e004bc8019b0f96d86d1ce7db1a597
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-16 18:01:52 -0400

    feat(edit-profile): create a page to edit the user profile with error handling when retrieving data

---
+++ src/app/@modal/edit-profile/page.tsx
@@ -0,0 +1,22 @@
+import { getUserProfile } from "@/src/lib/services/users"
+import ProfileUserFormContainer from "@/src/sections/users/form/profile/profile-user-form-container"
+import React from "react"
+
+type Props = {
+	params: Promise<{ id: string }>
+}
+
+export default async function EditProfilePage({ params }: Props) {
+	const { id } = await params
+	// get the profile
+	const res = await getUserProfile()
+
+	if (!res.response || res.error)
+		throw new Error("Error en la obtención del perfil de usuario")
+
+	return (
+		<div className="max-w-3xl overflow-auto">
+			<ProfileUserFormContainer user={res.response} />
+		</div>
+	)
+}
