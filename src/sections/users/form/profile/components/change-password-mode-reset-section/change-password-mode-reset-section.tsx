396bff1be8015d52467b4d4843e15d9f5685a3eb
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-16 18:03:58 -0400

    feat(change-password): remove the context of the modal and update the recovery link submit button to a navigation component

--- src/sections/users/form/profile/components/change-password-mode-reset-section/change-password-mode-reset-section.tsx
+++ src/sections/users/form/profile/components/change-password-mode-reset-section/change-password-mode-reset-section.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { ModalContext } from "@/src/components/modal/context/modalContext"
 import { Button } from "@/src/components/ui/button"
-import React, { Dispatch, SetStateAction, useContext } from "react"
+import React, { Dispatch, SetStateAction } from "react"
 import { User } from "@/src/lib/types/users"
+import NavigationComponent from "@/src/components/navigation-component/navigation-component"
+import { paths } from "@/src/lib/routes/paths"
 
 interface Props {
 	setChangePasswordMode: Dispatch<SetStateAction<"form" | "reset" | null>>
@@ -14,25 +15,13 @@ export default function ChangePasswordModeResetSection({
 	setChangePasswordMode,
 	user,
 }: Props) {
-	const { handleOpenModal } = useContext(ModalContext)
-	/* const { sendVerificationCode, loading, error } = useSendVerificationCode({
-    userId: user.id,
-    onSendVerificationCodeAction: () => {
-      toast.success("Código de verificación enviado, revise su correo");
-      handleOpenModal({
-        name: modalTypes.verifyCodeModal.name,
-        entity: user.id,
-      });
-    },
-  });*/
-
 	return (
 		<div className="space-y-4 p-4 border rounded-lg bg-muted/50">
-			{/*error && <AlertDestructive title={error} /> */}
 			<div className="flex items-center justify-between">
 				<h4 className="font-medium">Recuperar Contraseña</h4>
 				<Button
-					variant="ghost"
+					variant="secondary"
+					className="text-primary"
 					size="sm"
 					onClick={() => setChangePasswordMode(null)}
 				>
@@ -41,13 +30,25 @@ export default function ChangePasswordModeResetSection({
 			</div>
 
 			<div className="space-y-3">
-				<p className="text-sm text-muted-foreground">
+				<p className="text-sm text-secondary">
 					Se enviará un código de recuperación a: <br />
 					<strong>{user.email}</strong>
 				</p>
-				<Button onClick={() => {}} className="w-full">
-					Enviar Enlace de Recuperación
-				</Button>
+				<NavigationComponent
+					href={
+						paths.verificationCode({
+							id: user.id,
+							objective: "change-password",
+						}).root
+					}
+				>
+					<Button
+						variant={"secondary"}
+						className=" text-primary w-full"
+					>
+						Enviar Enlace de Recuperación
+					</Button>
+				</NavigationComponent>
 			</div>
 		</div>
 	)
