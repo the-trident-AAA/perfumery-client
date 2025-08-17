e32ce0db322624d14d503fa4333c759099cd36e3
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-17 10:30:28 -0400

    feat(forgot-password): add OTP verification and error handling on the password recovery page

--- src/app/@modal/forgot-password/[id]/[otp]/page.tsx
+++ src/app/@modal/forgot-password/[id]/[otp]/page.tsx
@@ -1,3 +1,4 @@
+import { checkOtp } from "@/src/lib/services/auth"
 import ForgotPasswordContainer from "@/src/sections/forgot-password/forgot-password-container"
 import React from "react"
 
@@ -7,6 +8,13 @@ type Props = {
 
 export default async function ForgotPasswordPage({ params }: Props) {
 	const { id, otp } = await params
+
+	// check the otp
+	const res = await checkOtp(id, otp)
+
+	if (!res.response || res.error)
+		return <div>No dispone del permiso para accerder a esta página</div>
+
 	return (
 		<div>
 			<ForgotPasswordContainer userId={id} otp={otp} />
