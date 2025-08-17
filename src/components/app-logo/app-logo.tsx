ec0013248b9110e3d5289af326f9cc0cc636a87c
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-17 11:27:01 -0400

    feat(app-logo): add properties to customize the avatar class and text display

--- src/components/app-logo/app-logo.tsx
+++ src/components/app-logo/app-logo.tsx
@@ -3,19 +3,29 @@ import NavigationComponent from "../navigation-component/navigation-component"
 import { paths } from "@/src/lib/routes/paths"
 import { Avatar, AvatarFallback, AvatarImage } from "@/src/components/ui/avatar"
 
-export default function AppLogo() {
+interface Props {
+	avatarClassName?: string
+	withText?: boolean
+}
+
+export default function AppLogo({
+	avatarClassName = "h-12 w-12",
+	withText = true,
+}: Props) {
 	return (
 		<NavigationComponent className="z-30" href={paths.home.root}>
 			<div className="flex items-center space-x-2">
-				<Avatar className="h-12 w-12">
+				<Avatar className={avatarClassName}>
 					<AvatarImage src="/icons/logo-icon.png" />
 					<AvatarFallback className="bg-secondary text-primary font-semibold text-sm">
 						{"PP"}
 					</AvatarFallback>
 				</Avatar>
-				<span className="text-xl hidden sm:flex font-bold text-secondary">
-					Perfumes del Puro
-				</span>
+				{withText && (
+					<span className="text-xl hidden sm:flex font-bold text-secondary">
+						Perfumes del Puro
+					</span>
+				)}
 			</div>
 		</NavigationComponent>
 	)
