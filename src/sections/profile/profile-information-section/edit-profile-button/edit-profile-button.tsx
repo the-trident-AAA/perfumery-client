a1fab16507678e57a4a447e77865ecd9b47382f7
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-16 23:07:06 -0400

    feat(edit-profile): replace the edit profile button with a navigation component that redirects to the edit profile page

--- src/sections/profile/profile-information-section/edit-profile-button/edit-profile-button.tsx
+++ src/sections/profile/profile-information-section/edit-profile-button/edit-profile-button.tsx
@@ -1,20 +1,17 @@
-"use client"
-import { ModalContext } from "@/src/components/modal/context/modalContext"
-import { modalTypes } from "@/src/components/modal/types/modalTypes"
+import NavigationComponent from "@/src/components/navigation-component/navigation-component"
 import { Button } from "@/src/components/ui/button"
-import React, { useContext } from "react"
+import { paths } from "@/src/lib/routes/paths"
+import React from "react"
 
 export default function EditProfileButton() {
-	const { handleOpenModal } = useContext(ModalContext)
 	return (
-		<Button
-			variant={"secondary"}
-			className="w-full py-3 px-4 text-primary"
-			onClick={() => {
-				handleOpenModal({ name: modalTypes.editProfileModal.name })
-			}}
-		>
-			Editar Perfil
-		</Button>
+		<NavigationComponent className="w-full" href={paths.editProfile.root}>
+			<Button
+				variant={"secondary"}
+				className="w-full py-3 px-4 text-primary"
+			>
+				Editar Perfil
+			</Button>
+		</NavigationComponent>
 	)
 }
