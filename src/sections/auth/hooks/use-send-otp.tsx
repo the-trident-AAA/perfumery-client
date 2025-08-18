57c6d6aed307c341b3e258d6d8ad3971ff7ed6dd
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-18 17:28:40 -0400

    refactor(use-send-otp): simplify the OTP sending function by removing the id parameter and making onSendAction optional

--- src/sections/auth/hooks/use-send-otp.tsx
+++ src/sections/auth/hooks/use-send-otp.tsx
@@ -3,34 +3,36 @@ import { sendOtp as sendOtpService } from "@/src/lib/services/auth"
 import { useCallback, useState } from "react"
 
 interface Props {
-	id: string
-	onSendAction: () => void
+	onSendAction?: () => void
 }
 
-export default function useSendOtp({ id, onSendAction }: Props) {
+export default function useSendOtp({ onSendAction }: Props) {
 	const [loading, setLoading] = useState(false)
 	const [error, setError] = useState<string | null>(null)
 
-	const sendOtp = useCallback(async () => {
-		try {
-			setLoading(true)
-			setError(null)
+	const sendOtp = useCallback(
+		async (id: string) => {
+			try {
+				setLoading(true)
+				setError(null)
 
-			const res = await sendOtpService(id)
-			if (!res.response || res.error)
-				setError(
-					res.error?.reason ||
-						"Error en el envío del código de verificación",
-				)
-			else {
-				onSendAction()
+				const res = await sendOtpService(id)
+				if (!res.response || res.error)
+					setError(
+						res.error?.reason ||
+							"Error en el envío del código de verificación",
+					)
+				else {
+					if (onSendAction) onSendAction()
+				}
+			} catch (error) {
+				console.log(error)
+			} finally {
+				setLoading(false)
 			}
-		} catch (error) {
-			console.log(error)
-		} finally {
-			setLoading(false)
-		}
-	}, [onSendAction, id])
+		},
+		[onSendAction],
+	)
 	return {
 		loading,
 		error,
