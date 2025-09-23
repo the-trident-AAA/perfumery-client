c87817a9331a6a6fef9749257f7c77f27d043a84
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-09-23 11:10:14 -0400

    refactor(sign-in-page): remove unnecessary properties from the SignInContainer component to simplify implementation.

--- src/app/(public)/sign-in/page.tsx
+++ src/app/(public)/sign-in/page.tsx
@@ -2,7 +2,5 @@ import SignInContainer from "@/src/sections/sign-in/sign-in-container"
 import React from "react"
 
 export default function SignInPage() {
-	return (
-		<SignInContainer logoText="Liux" imageSrc="/images/place-holder.jpg" />
-	)
+	return <SignInContainer />
 }
