aec78996fb83bc36d401e895048ee29d7dad7122
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-04 10:19:28 -0400

    refactor(add-shop-cart-perfume-button): eliminate the session dependency for adding perfumes to the cart, simplifying the cart management logic.

--- src/sections/perfumes/components/perfume-card/components/add-shop-cart-perfume-button.tsx
+++ src/sections/perfumes/components/perfume-card/components/add-shop-cart-perfume-button.tsx
@@ -1,13 +1,10 @@
 "use client"
 import { Button } from "@/src/components/ui/button"
-import { paths } from "@/src/lib/routes/paths"
 import { Perfume, PerfumeDetails } from "@/src/lib/types/perfumes"
 import { ShopCartContext } from "@/src/sections/shop-cart/context/shop-cart-context/shop-cart-context"
 import { ShopCartTotalItemsContext } from "@/src/sections/shop-cart/context/shop-cart-total-items-context/shop-cart-total-items-context"
 import useCreateShopCartPerfume from "@/src/sections/shop-cart/hooks/use-create-shop-cart-perfume"
 import { ShoppingCart } from "lucide-react"
-import { useSession } from "next-auth/react"
-import { useRouter } from "next/navigation"
 import React, { useCallback, useContext, useEffect } from "react"
 import { toast } from "react-toastify"
 
@@ -38,24 +35,17 @@ export default function AddShopCartPerfumeButton({
 				position: "bottom-center",
 			})
 	}, [error])
-	const router = useRouter()
-	const { data: session, status } = useSession()
 
 	const handleAddShopCartPerfume = useCallback(
 		(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
 			e.stopPropagation()
-			if (session) {
-				const shopCartId = session.user.shopCartId as string
-				createShopCartPerfume({
-					shopCartId,
-					perfumeId: perfume.id,
-					cant: 1,
-				})
-			} else {
-				router.push(paths.sign_in.root)
-			}
+
+			createShopCartPerfume({
+				perfumeId: perfume.id,
+				cant: 1,
+			})
 		},
-		[session, router],
+		[],
 	)
 
 	return (
