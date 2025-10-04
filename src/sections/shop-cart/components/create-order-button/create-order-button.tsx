91920669f8f98d2460caec17bcb3ba4c717c432c
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-04 11:16:34 -0400

    feat(create-order-button): add logic to redirect to the login page if the user is not authenticated when trying to create an order.

--- src/sections/shop-cart/components/create-order-button/create-order-button.tsx
+++ src/sections/shop-cart/components/create-order-button/create-order-button.tsx
@@ -2,10 +2,13 @@
 import { Button } from "@/src/components/ui/button"
 import { revalidateServerTags } from "@/src/lib/cache"
 import { tagsCacheByRoutes } from "@/src/lib/routes/api-routes/api-routes"
+import { paths } from "@/src/lib/routes/paths"
 import { UserTotalOrdersContext } from "@/src/sections/orders/context/user-total-orders-context"
 import useCreateOrder from "@/src/sections/orders/hooks/use-create-order"
 import { ShopCartContext } from "@/src/sections/shop-cart/context/shop-cart-context/shop-cart-context"
 import { ShopCartTotalItemsContext } from "@/src/sections/shop-cart/context/shop-cart-total-items-context/shop-cart-total-items-context"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
 import React, { useContext } from "react"
 import { toast } from "react-toastify"
 
@@ -14,6 +17,8 @@ interface Props {
 }
 
 export default function CreateOrderButton({ isDisabled = false }: Props) {
+	const { data: session } = useSession()
+	const router = useRouter()
 	const { fetchShopCartTotalItems } = useContext(ShopCartTotalItemsContext)
 	const { fetchShopCart } = useContext(ShopCartContext)
 	const { fetchUserTotalOrders } = useContext(UserTotalOrdersContext)
@@ -31,8 +36,8 @@ export default function CreateOrderButton({ isDisabled = false }: Props) {
 	return (
 		<Button
 			onClick={() => {
-				console.log("Se toco el click")
-				createOrder()
+				if (session) createOrder()
+				else router.push(paths.sign_in.root)
 			}}
 			variant={"secondary"}
 			className="text-primary lg:text-lg"
