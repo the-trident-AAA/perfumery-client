c0ea05ce2421869479a9d8748b1d1205a50e6636
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-04 10:40:26 -0400

    feat(sign-out-button): add logic to update the shopping cart when logging out, improving cart state management.

--- src/sections/root-layout/components/header/components/sign-out-button/sign-out-button.tsx
+++ src/sections/root-layout/components/header/components/sign-out-button/sign-out-button.tsx
@@ -1,15 +1,21 @@
 "use client"
 import { Button } from "@/src/components/ui/button"
 import { isProtectedRoute, paths } from "@/src/lib/routes/paths"
+import { ShopCartContext } from "@/src/sections/shop-cart/context/shop-cart-context/shop-cart-context"
+import { ShopCartTotalItemsContext } from "@/src/sections/shop-cart/context/shop-cart-total-items-context/shop-cart-total-items-context"
 import useSignOut from "@/src/sections/sign-in/form/hooks/use-sign-out"
 import { usePathname, useRouter } from "next/navigation"
-import React, { useCallback } from "react"
+import React, { useCallback, useContext } from "react"
 
 export default function SignOutButton() {
 	const router = useRouter()
 	const currentPath = usePathname()
+	const { fetchShopCartTotalItems } = useContext(ShopCartTotalItemsContext)
+	const { fetchShopCart } = useContext(ShopCartContext)
 	const { signOut, loading } = useSignOut({
 		onSignOutAction: () => {
+			fetchShopCartTotalItems()
+			fetchShopCart()
 			if (isProtectedRoute(currentPath)) router.push(paths.home.root)
 		},
 	})
