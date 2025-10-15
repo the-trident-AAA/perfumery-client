6d8faf7d95d20f3bb9053eed97b7d8304fdfb9ec
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-15 16:54:02 -0400

    feat(orders-messages): remove unnecessary itemsIntro variable to simplify order message formatting

--- src/sections/orders/lib/orders-messages.ts
+++ src/sections/orders/lib/orders-messages.ts
@@ -11,15 +11,13 @@ export function formatOrderMessage(
 
 	const header = `*Pedido*: ${order.id}\n*Email*: ${order.user.email}\n\n`
 
-	const itemsIntro = `*** Perfumes del Pedido *** \n\n`
-
 	const items = order.orderPerfumes
 		.map((item, index) => {
 			const total = item.price
 			const scents =
 				item.perfume.scents?.map((s: any) => s.name).join(", ") ||
 				"Sin especificar"
-			return `*${index + 1}.* ${item.perfume.name} - ${item.perfume.brand.name}\n    Cantidad: ${item.cant}\n    Precio: $${item.perfume.totalPrice}\n    Subtotal: $${total}\n    Aromas: ${scents}`
+			return `*${index + 1}.* ${item.perfume.name} - ${item.perfume.brand.name}\n    Cantidad: ${item.cant}\n    Precio: $${item.perfume.totalPrice}\n    Subtotal: $${total}\n    `
 		})
 		.join("\n\n")
 
@@ -27,5 +25,5 @@ export function formatOrderMessage(
 
 	const footer = `\n\n *Total del pedido*: $${totalGeneral}`
 
-	return intro + header + itemsIntro + items + footer
+	return intro + header + items + footer
 }
