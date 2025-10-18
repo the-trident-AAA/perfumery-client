e45ceb3b3fc2d6296e6dec6ed1c5a70dcae75302
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-18 17:46:32 -0400

    feat(navigation-component): add support for opening links in a new tab and customizable scroll behavior

--- src/components/navigation-component/navigation-component.tsx
+++ src/components/navigation-component/navigation-component.tsx
@@ -4,16 +4,28 @@ import React, { ReactNode } from "react"
 interface Props {
 	children: ReactNode
 	href: string
+	inAnotherTab?: boolean
 	className?: string
+	scroll?: boolean
 }
 
 export default function NavigationComponent({
 	children,
 	href,
+	inAnotherTab = false,
 	className,
+	scroll = true,
 }: Props) {
 	return (
-		<Link className={className} href={href}>
+		<Link
+			href={href}
+			scroll={scroll}
+			{...(inAnotherTab && {
+				target: "_blank",
+				rel: "noopener noreferrer",
+			})}
+			className={className}
+		>
 			{children}
 		</Link>
 	)
