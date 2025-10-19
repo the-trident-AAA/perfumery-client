56320fdc4d98d44267a85690975d87261a4a4875
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-18 23:47:48 -0400

    fix(confirm-email): enhance onVerifyEmailAction to show success toast and redirect after verification

--- src/sections/confirm-email/form/confirm-email-form-container.tsx
+++ src/sections/confirm-email/form/confirm-email-form-container.tsx
@@ -2,6 +2,7 @@
 import { AlertDestructive } from "@/src/components/ui/alert-destructive"
 import { Button } from "@/src/components/ui/button"
 import { Form } from "@/src/components/ui/form"
+import { paths } from "@/src/lib/routes/paths"
 import ConfirmEmailForm from "@/src/sections/confirm-email/form/confirm-email-form"
 import {
 	confirmEmailSchema,
@@ -9,16 +10,27 @@ import {
 } from "@/src/sections/confirm-email/form/schemas/confirm-email-schema"
 import useVerifyEmail from "@/src/sections/confirm-email/hooks/use-verify-email"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useRouter } from "next/navigation"
 import React from "react"
 import { useForm } from "react-hook-form"
+import { toast } from "react-toastify"
 
 export default function ConfirmEmailFormContainer() {
+	const router = useRouter()
 	const {
 		verifyEmail,
 		loading: loadingVerifyEmail,
 		error: verifyEmailError,
 	} = useVerifyEmail({
-		onVerifyEmailAction: () => {},
+		onVerifyEmailAction: userId => {
+			toast.success("Identidad verificada con éxito")
+			router.push(
+				paths.verificationCode({
+					id: userId,
+					objective: "change-password",
+				}).root,
+			)
+		},
 	})
 
 	const form = useForm<ConfirmEmailSchema>({
