ea0193942cc599b4873733773ce1bf24681e4ed6
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-19 12:57:53 -0400

    feat(orders): update OrdersList to accept search parameters for dynamic order fetching

--- src/sections/orders/orders-list/orders-list.tsx
+++ src/sections/orders/orders-list/orders-list.tsx
@@ -1,14 +1,19 @@
 import EmptyContent from "@/src/components/empty-content/empty-content"
 import { getOrdersList } from "@/src/lib/services/orders"
+import { SearchParamsPagination } from "@/src/lib/types/pagination"
 import OrderCard from "@/src/sections/orders/order-card/order-card"
 import React from "react"
 
 interface Props {
 	variant?: "modal" | "default"
+	searchParams: SearchParamsPagination
 }
 
-export default async function OrdersList({ variant = "default" }: Props) {
-	const res = await getOrdersList({ limit: 100 })
+export default async function OrdersList({
+	variant = "default",
+	searchParams,
+}: Props) {
+	const res = await getOrdersList(searchParams)
 
 	if (!res.response || res.error)
 		return <div>{res.error?.reason || "Error en la carga de orders"}</div>
