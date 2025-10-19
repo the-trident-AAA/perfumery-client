c2c370413d2d4c6fca81bc35a1315651463d9871
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-18 23:22:16 -0400

    feat(confirm-email): implement custom hook for email verification logic

---
+++ src/sections/confirm-email/hooks/use-verify-email.ts
@@ -0,0 +1,45 @@
+"use client"
+import { verifyEmail as verifyEmailService } from "@/src/lib/services/auth"
+import { convertVerifyEmailDTO } from "@/src/lib/types/auth"
+import { ConfirmEmailSchema } from "@/src/sections/confirm-email/form/schemas/confirm-email-schema"
+import { useCallback, useState } from "react"
+
+interface Props {
+	onVerifyEmailAction: () => Promise<void>
+}
+
+export default function useVerifyEmail({ onVerifyEmailAction }: Props) {
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState<string | null>(null)
+
+	const verifyEmail = useCallback(
+		async (confirmEmail: ConfirmEmailSchema) => {
+			try {
+				setLoading(true)
+				setError(null)
+
+				const res = await verifyEmailService(
+					convertVerifyEmailDTO(confirmEmail),
+				)
+				if (!res.response || res.error)
+					setError(
+						res.error?.reason ||
+							"Error en la verificación del email",
+					)
+				else {
+					onVerifyEmailAction()
+				}
+			} catch (error) {
+				console.log(error)
+			} finally {
+				setLoading(false)
+			}
+		},
+		[onVerifyEmailAction],
+	)
+	return {
+		loading,
+		error,
+		verifyEmail,
+	}
+}
