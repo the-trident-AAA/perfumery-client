b3e20b7b10d9f7fa71ace8564cb821a350cf5be9
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-20 11:27:13 -0400

    feat(orders): integrate OrdersOrderContainer into OrdersList for enhanced order display

--- src/sections/orders/orders-list/orders-list.tsx
+++ src/sections/orders/orders-list/orders-list.tsx
@@ -3,6 +3,7 @@ import { getOrdersList } from "@/src/lib/services/orders"
 import { SearchParamsPagination } from "@/src/lib/types/pagination"
 import OrdersFiltersContainer from "@/src/sections/orders/filters/orders-filters-container"
 import OrderCard from "@/src/sections/orders/order-card/order-card"
+import OrdersOrderContainer from "@/src/sections/orders/order/orders-order-container"
 import React from "react"
 
 interface Props {
@@ -24,6 +25,7 @@ export default async function OrdersList({
 	return (
 		<div className="flex flex-col gap-4">
 			<OrdersFiltersContainer />
+			<OrdersOrderContainer />
 			<div className="grid grid-cols-1 gap-4 p-2">
 				{orders.length > 0 ? (
 					orders.map(order => (
