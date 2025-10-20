2239dcf9607fdecd7f8a255f6abe6997cb1159e0
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-20 11:33:51 -0400

    fix(sorter-component): adjust text size for sort labels to improve readability

--- src/components/sorter-component/sorter-component.tsx
+++ src/components/sorter-component/sorter-component.tsx
@@ -103,7 +103,9 @@ export default function SorterComponent({
 					onClick={() => handleSortClick(option.key)}
 					className={`flex items-center ${currentSort === option.key && "text-primary"} gap-1`}
 				>
-					<p className="text-base">{getSortLabel(option)}</p>
+					<p className="text-sm sm:text-base">
+						{getSortLabel(option)}
+					</p>
 					{getSortIcon(option.key)}
 				</Button>
 			))}
