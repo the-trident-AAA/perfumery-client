73e2008bba89bff2dd1d9195f07cb9dffc9b0de1
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-22 22:44:35 -0400

    feat(best-selling-products): update to use getBestSellersPerfumes for fetching best-selling perfumes

--- src/sections/home/components/best-selling-products/list/best-selling-products-list.tsx
+++ src/sections/home/components/best-selling-products/list/best-selling-products-list.tsx
@@ -1,12 +1,13 @@
 import EmptyContent from "@/src/components/empty-content/empty-content"
-import { getPerfumesList } from "@/src/lib/services/perfumes"
+import { getBestSellersPerfumes } from "@/src/lib/services/perfumes"
 import HomePromotionsCarousel from "@/src/sections/home/components/home-promotions-carousel/home-promotions-carousel"
 import React from "react"
 
 export default async function BestSellingProductsList() {
-	const res = await getPerfumesList({})
-	if (!res.response || res.error) throw new Error("Error fetching perfumes")
-	const perfumes = res.response.data
+	const res = await getBestSellersPerfumes()
+	if (!res.response || res.error)
+		throw new Error("Error fetching best sellers perfumes")
+	const perfumes = res.response
 	return perfumes.length > 0 ? (
 		<HomePromotionsCarousel data={perfumes} />
 	) : (
