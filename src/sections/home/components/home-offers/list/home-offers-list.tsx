25815cf9eb03d0e73aab547f3cb3679f6af4e603
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-30 20:51:09 -0400

    feat(home-offers): implement carousel for offers list and enhance layout for better responsiveness

--- src/sections/home/components/home-offers/list/home-offers-list.tsx
+++ src/sections/home/components/home-offers/list/home-offers-list.tsx
@@ -1,6 +1,8 @@
 import EmptyContent from "@/src/components/empty-content/empty-content"
+import StandardCarousel from "@/src/components/ui/standard-carousel/standard-carousel"
 import { getOffersList } from "@/src/lib/services/offers"
 import OfferCard from "@/src/sections/home/components/home-offers/components/offer-card/offer-card"
+import HomeOffersListCarousel from "@/src/sections/home/components/home-offers/list/home-offers-list-carousel"
 import React from "react"
 
 export default async function HomeOffersList() {
@@ -8,11 +10,7 @@ export default async function HomeOffersList() {
 	if (!res.response || res.error) throw new Error("Error in fetching offers")
 	const offers = res.response
 	return offers.length > 0 ? (
-		<div className="grid sm:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-8">
-			{offers.map((offer, index) => (
-				<OfferCard key={index} offer={offer} />
-			))}
-		</div>
+		<HomeOffersListCarousel data={offers} />
 	) : (
 		<EmptyContent
 			title="No hay ofertas nuevas"
