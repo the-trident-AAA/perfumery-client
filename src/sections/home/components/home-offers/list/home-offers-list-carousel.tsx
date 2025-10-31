b45309c561288860e69e654889fd6a08759a864b
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-30 20:51:09 -0400

    feat(home-offers): implement carousel for offers list and enhance layout for better responsiveness

---
+++ src/sections/home/components/home-offers/list/home-offers-list-carousel.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import StandardCarousel from "@/src/components/ui/standard-carousel/standard-carousel"
+import { Offer } from "@/src/lib/types/offers"
+
+import OfferCard from "@/src/sections/home/components/home-offers/components/offer-card/offer-card"
+
+import React from "react"
+
+interface Props {
+	data: Offer[]
+}
+
+export default function HomeOffersListCarousel({ data }: Props) {
+	return (
+		<StandardCarousel
+			items={data}
+			itemClassName=" sm:basis-[70%] md:basis-1/2 xl:basis-1/3 "
+			className="pl-4 mr-4"
+			loop={false}
+			renderCard={item => <OfferCard offer={item} />}
+		/>
+	)
+}
