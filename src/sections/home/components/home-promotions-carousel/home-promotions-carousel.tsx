897fa66f22fc1b1eb4e0d87f806a75a58ca656fa
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-31 01:30:20 -0400

    fix(home-promotions-carousel): adjust item class name for improved layout consistency

--- src/sections/home/components/home-promotions-carousel/home-promotions-carousel.tsx
+++ src/sections/home/components/home-promotions-carousel/home-promotions-carousel.tsx
@@ -14,8 +14,7 @@ export default function HomePromotionsCarousel({ data }: Props) {
 	return (
 		<StandardCarousel
 			items={data}
-			itemClassName=" basis-[78%]  2xs:basis-[50%]  sm:basis-[40%] md:basis-1/3 lg:basis-1/4 xl:basis-1/5 "
-			className="pl-4 mr-4"
+			itemClassName="basis-[50%] 2xs:basis-[50%]  sm:basis-[40%] md:basis-1/3 lg:basis-1/4 xl:basis-1/5 "
 			loop={false}
 			renderCard={item => <PerfumeCard perfume={item} />}
 		/>
