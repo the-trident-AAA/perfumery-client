cc3251fc65012d4ba1d35e11668f6e48856350ce
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-30 20:50:50 -0400

    fix(home-promotions-carousel): simplify carousel structure and improve item layout for better responsiveness

--- src/sections/home/components/home-promotions-carousel/home-promotions-carousel.tsx
+++ src/sections/home/components/home-promotions-carousel/home-promotions-carousel.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { StandardCarouselProvider } from "@/src/components/ui/standard-carousel/context/standard-carousel-context"
 import StandardCarousel from "@/src/components/ui/standard-carousel/standard-carousel"
 import { Perfume } from "@/src/lib/types/perfumes"
 import PerfumeCard from "@/src/sections/perfumes/components/perfume-card/perfume-card"
@@ -13,30 +12,12 @@ interface Props {
 
 export default function HomePromotionsCarousel({ data }: Props) {
 	return (
-		<StandardCarouselProvider>
-			<StandardCarousel
-				items={data}
-				dimension="100vw"
-				className="pl-4 mr-4"
-				itemsStyles="sm:basis-1/2 md:basis-1/3 lg:basis-1/3 2xl:basis-1/4"
-				renderCard={item => <PerfumeCard perfume={item} />}
-				shouldCenter={(breakpoint: string, cantElements: number) => {
-					switch (breakpoint) {
-						case "3xl":
-							return cantElements < 3
-						case "2xl":
-							return cantElements < 3
-						case "xl":
-							return cantElements < 3
-						case "lg":
-							return cantElements < 3
-						case "md":
-							return cantElements < 3
-						default:
-							return cantElements < 2
-					}
-				}}
-			/>
-		</StandardCarouselProvider>
+		<StandardCarousel
+			items={data}
+			itemClassName=" basis-[78%]  2xs:basis-[50%]  sm:basis-[40%] md:basis-1/3 lg:basis-1/4 xl:basis-1/5 "
+			className="pl-4 mr-4"
+			loop={false}
+			renderCard={item => <PerfumeCard perfume={item} />}
+		/>
 	)
 }
