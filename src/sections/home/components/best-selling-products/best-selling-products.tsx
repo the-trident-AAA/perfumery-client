6a5676fc91194d5bb99f35931b0a40ced72832aa
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-31 01:30:07 -0400

    fix(best-selling-products): update padding for improved layout responsiveness

--- src/sections/home/components/best-selling-products/best-selling-products.tsx
+++ src/sections/home/components/best-selling-products/best-selling-products.tsx
@@ -41,7 +41,7 @@ export default async function BestSellingProducts() {
 
 			{/* Contenido principal */}
 			<div className="relative z-10 pt-20 pb-24">
-				<div className="lg:container lg:mx-auto px-6">
+				<div className="lg:container lg:mx-auto 2xs:px-6 px-4">
 					{/* Header de la sección mejorado */}
 					<div className="text-center space-y-8 mb-16">
 						{/* Badge superior */}
