c7b17a6af9f9d92260c1332db9cbfd3eefdff47a
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-31 21:29:45 -0400

    feat(quick-links): integrate dynamic quick link rendering with scroll support

--- src/sections/root-layout/components/footer/components/quick-links/quick-links.tsx
+++ src/sections/root-layout/components/footer/components/quick-links/quick-links.tsx
@@ -1,38 +1,41 @@
+"use client"
+import useQuickLinks from "@/src/lib/hooks/quick-links/use-quick-links"
 import Link from "next/link"
 import React from "react"
+import { Link as LinkScrollReact } from "react-scroll"
 
 export default function QuickLinks() {
+	const { quickLinks } = useQuickLinks()
 	return (
 		<div className="space-y-4">
 			<h4 className="font-medium text-secondary text-sm uppercase tracking-wider">
-				Shop
+				Tienda
 			</h4>
 			<ul className="space-y-2 text-sm">
-				<li>
-					<Link href="#" className="text-secondary hover:text-white">
-						Women's Perfumes
-					</Link>
-				</li>
-				<li>
-					<Link href="#" className="text-secondary hover:text-white">
-						Men's Colognes
-					</Link>
-				</li>
-				<li>
-					<Link href="#" className="text-secondary hover:text-white">
-						Gift Sets
-					</Link>
-				</li>
-				<li>
-					<Link href="#" className="text-secondary hover:text-white">
-						New Arrivals
-					</Link>
-				</li>
-				<li>
-					<Link href="#" className="text-secondary hover:text-white">
-						Best Sellers
-					</Link>
-				</li>
+				{quickLinks.map((quickLink, index) => (
+					<li key={index}>
+						{quickLink.isScrollReact ? (
+							<LinkScrollReact
+								key={index}
+								to={quickLink.href}
+								spy={true}
+								smooth={true}
+								offset={-100}
+								duration={500}
+								className="cursor-pointer transition-all duration-300 hover:font-bold"
+							>
+								{quickLink.label}
+							</LinkScrollReact>
+						) : (
+							<Link
+								href={quickLink.href}
+								className="cursor-pointer transition-all duration-300 hover:font-bold"
+							>
+								{quickLink.label}
+							</Link>
+						)}
+					</li>
+				))}
 			</ul>
 		</div>
 	)
