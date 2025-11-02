a825664f65ddb896002939facf5dc7dd6c6b4e38
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-02 14:41:24 -0500

    feat(quick-links): add dynamic rendering of perfume types to quick links

--- src/lib/hooks/quick-links/use-quick-links.tsx
+++ src/lib/hooks/quick-links/use-quick-links.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { paths } from "@/src/lib/routes/paths"
+import usePerfumeTypes from "@/src/sections/perfume-types/hooks/use-perfume-types"
 import { usePathname } from "next/navigation"
 import { useMemo } from "react"
 
@@ -7,53 +8,67 @@ export interface QuickLink {
 	label: string
 	href: string
 	isScrollReact: boolean
+	children?: QuickLink
 }
 
 export default function useQuickLinks() {
 	const pathname = usePathname()
-
+	const { perfumeTypes, loadingData: loadingDataPerfumesType } =
+		usePerfumeTypes()
 	const quickLinks: QuickLink[] = useMemo(
-		() => [
-			{
-				label: "Inicio",
-				href:
-					pathname === paths.home.root
-						? "home-hero"
-						: paths.home.root + "#home-hero",
-				isScrollReact: pathname === paths.home.root ? true : false,
-			},
-			{
-				label: "Ofertas",
-				href:
-					pathname === paths.home.root
-						? "home-offers"
-						: paths.home.root + "#home-offers",
-				isScrollReact: pathname === paths.home.root ? true : false,
-			},
-			{
-				label: "Más vendidos",
-				href:
-					pathname === paths.home.root
-						? "best-selling"
-						: paths.home.root + "#best-selling",
-				isScrollReact: pathname === paths.home.root ? true : false,
-			},
-			{
-				label: "Tipos de Perfumes",
-				href:
-					pathname === paths.home.root
-						? "home-perfume-groups"
-						: paths.home.root + "#home-perfume-groups",
-				isScrollReact: pathname === paths.home.root ? true : false,
-			},
-			{
-				label: "Perfumes",
-				href: paths.perfumes().root,
-				isScrollReact: false,
-			},
-		],
+		() =>
+			[
+				{
+					label: "Inicio",
+					href:
+						pathname === paths.home.root
+							? "home-hero"
+							: paths.home.root + "#home-hero",
+					isScrollReact: pathname === paths.home.root ? true : false,
+				},
+				{
+					label: "Ofertas",
+					href:
+						pathname === paths.home.root
+							? "home-offers"
+							: paths.home.root + "#home-offers",
+					isScrollReact: pathname === paths.home.root ? true : false,
+				},
+				{
+					label: "Más vendidos",
+					href:
+						pathname === paths.home.root
+							? "best-selling"
+							: paths.home.root + "#best-selling",
+					isScrollReact: pathname === paths.home.root ? true : false,
+				},
+				{
+					label: "Tipos de Perfumes",
+					href:
+						pathname === paths.home.root
+							? "home-perfume-groups"
+							: paths.home.root + "#home-perfume-groups",
+					isScrollReact: pathname === paths.home.root ? true : false,
+				},
+				{
+					label: "Perfumes",
+					href: paths.perfumes().root,
+					isScrollReact: false,
+				},
+				{
+					label: "Fragancias",
+					href: paths.perfumes().root,
+					isScrollReact: false,
+					children: perfumeTypes.map(perfumeType => ({
+						label: perfumeType.name,
+						href: paths.perfumes({ perfumeTypeId: perfumeType.id })
+							.root,
+						isScrollReact: false,
+					})),
+				},
+			] as QuickLink[],
 		[pathname, paths],
 	)
 
-	return { quickLinks, pathname }
+	return { quickLinks, pathname, loadingDataPerfumesType }
 }
