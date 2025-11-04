eaa2aee7adc98a4823fbf77432d500509c777a38
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-03 20:49:27 -0500

    feat(orders): update OrdersList to mark orders as viewed and handle errors on component mount

--- src/sections/orders/orders-list/orders-list.tsx
+++ src/sections/orders/orders-list/orders-list.tsx
@@ -1,7 +1,11 @@
 import EmptyContent from "@/src/components/empty-content/empty-content"
-import { getOrdersList } from "@/src/lib/services/orders"
+import {
+	getOrdersList,
+	updateUserOrdersAsViews,
+} from "@/src/lib/services/orders"
 import { SearchParamsPagination } from "@/src/lib/types/pagination"
 import OrdersFiltersContainer from "@/src/sections/orders/filters/orders-filters-container"
+import OrdersNotSeenByUserContextHandler from "@/src/sections/orders/handlers/orders-not-seen-by-user-context-handler"
 import OrderCard from "@/src/sections/orders/order-card/order-card"
 import OrdersOrderContainer from "@/src/sections/orders/order/orders-order-container"
 import React from "react"
@@ -15,6 +19,16 @@ export default async function OrdersList({
 	variant = "default",
 	searchParams,
 }: Props) {
+	const ordersNotSeenByUserRes = await updateUserOrdersAsViews()
+
+	if (!ordersNotSeenByUserRes.response || ordersNotSeenByUserRes.error)
+		return (
+			<div>
+				{ordersNotSeenByUserRes.error?.reason ||
+					"Error en la actualización"}
+			</div>
+		)
+
 	const res = await getOrdersList(searchParams)
 
 	if (!res.response || res.error)
@@ -24,6 +38,7 @@ export default async function OrdersList({
 
 	return (
 		<div className="flex flex-col gap-4">
+			<OrdersNotSeenByUserContextHandler />
 			<OrdersFiltersContainer />
 			<OrdersOrderContainer />
 			<div className="grid grid-cols-1 gap-4 p-2">
