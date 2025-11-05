2e65cd5316440680e1879e207bdc63f98a703ea6
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-05 15:42:20 -0500

    feat(format-date): enhance date formatting function with predefined formats and custom options

--- src/lib/format-date.ts
+++ src/lib/format-date.ts
@@ -1,10 +1,98 @@
-export const formatDate = (dateString: string) => {
+type PredefinedFormat =
+	| "short"
+	| "long"
+	| "numeric"
+	| "monthYear"
+	| "year"
+	| "time"
+	| "datetime"
+	| "full"
+	| "yymmdd"
+
+interface CustomFormatOptions {
+	locale?: string
+	year?: "numeric" | "2-digit"
+	month?: "numeric" | "2-digit" | "long" | "short" | "narrow"
+	day?: "numeric" | "2-digit"
+	hour?: "numeric" | "2-digit"
+	minute?: "numeric" | "2-digit"
+	second?: "numeric" | "2-digit"
+	weekday?: "long" | "short" | "narrow"
+	timeZone?: string
+}
+
+export const formatDate = (
+	dateString: string,
+	format: PredefinedFormat | CustomFormatOptions = "long",
+) => {
 	const date = new Date(dateString)
-	return date.toLocaleDateString("es-ES", {
-		year: "numeric",
-		month: "long",
-		day: "numeric",
-	})
+
+	const predefinedFormats: Record<PredefinedFormat, CustomFormatOptions> = {
+		short: {
+			year: "numeric",
+			month: "short",
+			day: "numeric",
+		},
+		long: {
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+		},
+		numeric: {
+			year: "numeric",
+			month: "numeric",
+			day: "numeric",
+		},
+		monthYear: {
+			year: "numeric",
+			month: "long",
+		},
+		year: {
+			year: "numeric",
+		},
+		time: {
+			hour: "2-digit",
+			minute: "2-digit",
+			second: "2-digit",
+		},
+		datetime: {
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+			hour: "2-digit",
+			minute: "2-digit",
+		},
+		full: {
+			weekday: "long",
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+			hour: "2-digit",
+			minute: "2-digit",
+			timeZone: "UTC",
+		},
+		yymmdd: {
+			year: "2-digit",
+			month: "2-digit",
+			day: "2-digit",
+		},
+	}
+
+	// Si es el formato yymmdd, necesitamos un manejo especial para asegurar el formato YY/MM/DD
+	if (format === "yymmdd") {
+		const year = date.getFullYear().toString().slice(-2) // Últimos 2 dígitos del año
+		const month = (date.getMonth() + 1).toString().padStart(2, "0") // Mes con 2 dígitos
+		const day = date.getDate().toString().padStart(2, "0") // Día con 2 dígitos
+
+		return `${year}/${month}/${day}`
+	}
+
+	const options =
+		typeof format === "string" ? predefinedFormats[format] : format
+
+	const defaultLocale = "es-ES"
+
+	return date.toLocaleDateString(options.locale || defaultLocale, options)
 }
 
 export const getDateDuration = (startDate: string, endDate: string) => {
