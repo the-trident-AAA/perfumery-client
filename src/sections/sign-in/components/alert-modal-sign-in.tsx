666491ba458686b8ce34603d794d2190f0f34e27
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-07 17:50:35 -0500

    fix(alert-modal): correct spelling in AlertModalSignIn title for improved clarity

--- src/sections/sign-in/components/alert-modal-sign-in.tsx
+++ src/sections/sign-in/components/alert-modal-sign-in.tsx
@@ -6,7 +6,7 @@ export default function AlertModalSignIn() {
 	const [openModal, setOpenModal] = useState(true)
 	return (
 		<AlertModal
-			title="Es necesara autenticación para crear un Pedido"
+			title="Es necesaria autenticación para crear un Pedido"
 			description="Es necesario haber iniciado sesión para poder crear un pedido y hacer uso de ese beneficio."
 			isOpen={openModal}
 			onClose={() => {
