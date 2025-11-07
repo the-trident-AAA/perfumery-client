7a8e044b0913f398f793d67c526cfb2eed3cecd9
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-07 11:07:16 -0500

    feat(scents): add ScentFiltersDTO interface and conversion function for scent filters

--- src/lib/types/scents.ts
+++ src/lib/types/scents.ts
@@ -1,4 +1,16 @@
+import { ScentsFilters } from "@/src/sections/scents/filters/hooks/use-scents-filters"
+
 export interface Scent {
 	id: string
 	name: string
+}
+
+export interface ScentFiltersDTO {
+	name?: string
+}
+
+export const convertScentFiltersDTO = (
+	scentFilters: ScentsFilters,
+): ScentFiltersDTO => {
+	return { ...scentFilters }
 }
