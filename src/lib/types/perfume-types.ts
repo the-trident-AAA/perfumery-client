dabd543f66c3f0cf3992c521354aea168d5a877a
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-07 11:16:51 -0500

    feat(perfume-types-filters): add PerfumeTypeFiltersDTO and enhance usePerfumeTypesFilters with URL filter updates and active filters count

--- src/lib/types/perfume-types.ts
+++ src/lib/types/perfume-types.ts
@@ -1,5 +1,17 @@
+import { PerfumeTypesFilters } from "@/src/sections/perfume-types/filters/hooks/use-perfume-types-filters"
+
 export interface PerfumeType {
 	id: string
 	name: string
 	image?: string
+}
+
+export interface PerfumeTypeFiltersDTO {
+	name?: string
+}
+
+export const convertPerfumeTypeFiltersDTO = (
+	perfumeTypesFilters: PerfumeTypesFilters,
+): PerfumeTypeFiltersDTO => {
+	return { ...perfumeTypesFilters }
 }
--- src/sections/perfume-types/filters/hooks/use-perfume-types-filters.tsx
+++ src/sections/perfume-types/filters/hooks/use-perfume-types-filters.tsx
@@ -1,37 +1,59 @@
 "use client"
 
+import useUrlFilters from "@/src/lib/hooks/use-url-filters"
 import { Pagination } from "@/src/lib/types/pagination"
+import { convertPerfumeTypeFiltersDTO } from "@/src/lib/types/perfume-types"
 import { Dispatch, SetStateAction, useState } from "react"
 
-interface PerfumeTypesFilters {
-	search: string
+export interface PerfumeTypesFilters {
+	name?: string
 }
 
 interface Props {
 	setPagination?: Dispatch<SetStateAction<Pagination>>
+	urlFilters?: boolean
 }
 
-export default function usePerfumeTTypesFilters({ setPagination }: Props) {
-	const [filters, setFilters] = useState<PerfumeTypesFilters>({
-		search: "",
-	})
+export default function usePerfumeTypesFilters({
+	setPagination,
+	urlFilters = true,
+}: Props) {
+	const { updateFiltersInUrl } = useUrlFilters()
+	const [filters, setFilters] = useState<PerfumeTypesFilters>({})
 
 	async function handleChangeFilters(updatedFilters: PerfumeTypesFilters) {
+		const newFilters = {
+			...filters,
+			...updatedFilters,
+		}
 		await setFilters(prev => ({
 			...prev,
 			...updatedFilters,
 		}))
+		if (urlFilters)
+			updateFiltersInUrl(convertPerfumeTypeFiltersDTO(newFilters))
 		if (setPagination)
 			setPagination(oldPagination => ({ ...oldPagination, page: 1 }))
 	}
 
 	function handleResetFilters() {
-		setFilters({
-			search: "",
-		})
+		setFilters({})
+		if (urlFilters) updateFiltersInUrl({})
 		if (setPagination)
 			setPagination(oldPagination => ({ ...oldPagination, page: 1 }))
 	}
 
-	return { filters, handleChangeFilters, handleResetFilters }
+	const getActiveFiltersCount = () => {
+		let count = 0
+		if (filters.name) count++
+
+		return count
+	}
+
+	return {
+		filters,
+		handleChangeFilters,
+		handleResetFilters,
+		getActiveFiltersCount,
+	}
 }
