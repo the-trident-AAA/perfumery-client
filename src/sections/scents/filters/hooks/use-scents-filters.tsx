f44dbcab0e348792a38b29fa64f6c4f8b5b69ae2
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-07 11:07:06 -0500

    feat(scents-filters): enhance useScentsFilters hook with URL filter updates and active filters count

--- src/sections/scents/filters/hooks/use-scents-filters.tsx
+++ src/sections/scents/filters/hooks/use-scents-filters.tsx
@@ -1,37 +1,57 @@
 "use client"
-
+import useUrlFilters from "@/src/lib/hooks/use-url-filters"
 import { Pagination } from "@/src/lib/types/pagination"
+import { convertScentFiltersDTO } from "@/src/lib/types/scents"
 import { Dispatch, SetStateAction, useState } from "react"
 
-interface ScentsFilters {
-	search: string
+export interface ScentsFilters {
+	name?: string
 }
 
 interface Props {
 	setPagination?: Dispatch<SetStateAction<Pagination>>
+	urlFilters?: boolean
 }
 
-export default function useScentsFilters({ setPagination }: Props) {
-	const [filters, setFilters] = useState<ScentsFilters>({
-		search: "",
-	})
+export default function useScentsFilters({
+	setPagination,
+	urlFilters = true,
+}: Props) {
+	const { updateFiltersInUrl } = useUrlFilters()
+	const [filters, setFilters] = useState<ScentsFilters>({})
 
 	async function handleChangeFilters(updatedFilters: ScentsFilters) {
+		const newFilters = {
+			...filters,
+			...updatedFilters,
+		}
 		await setFilters(prev => ({
 			...prev,
 			...updatedFilters,
 		}))
+		if (urlFilters) updateFiltersInUrl(convertScentFiltersDTO(newFilters))
 		if (setPagination)
 			setPagination(oldPagination => ({ ...oldPagination, page: 1 }))
 	}
 
 	function handleResetFilters() {
-		setFilters({
-			search: "",
-		})
+		setFilters({})
+		if (urlFilters) updateFiltersInUrl({})
 		if (setPagination)
 			setPagination(oldPagination => ({ ...oldPagination, page: 1 }))
 	}
 
-	return { filters, handleChangeFilters, handleResetFilters }
+	const getActiveFiltersCount = () => {
+		let count = 0
+		if (filters.name) count++
+
+		return count
+	}
+
+	return {
+		filters,
+		handleChangeFilters,
+		handleResetFilters,
+		getActiveFiltersCount,
+	}
 }
