c5a964f7c32c37c428a201b434f1f28aa93c1f57
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-09 19:52:37 -0500

    fix(useModal): prevent duplicate modals and streamline modal closing logic

--- src/components/modal/hooks/useModal.tsx
+++ src/components/modal/hooks/useModal.tsx
@@ -6,22 +6,25 @@ export default function useModal() {
 
 	const handleOpenModal = (infoModal: InfoModal) => {
 		if (infoModal.onOpen) infoModal.onOpen()
-		setCurrentModal([...currentModal, infoModal])
+
+		setCurrentModal(prev => {
+			// Evitar duplicados (opcional)
+			if (prev.some(modal => modal.name === infoModal.name)) return prev
+			return [...prev, infoModal]
+		})
 	}
 
 	const handleCloseModal = (
 		modalName: string,
 		callOnClose: boolean = true,
 	) => {
-		const modalToClose = currentModal.find(
-			modal => modal.name === modalName,
-		)
-		if (modalToClose) {
-			if (modalToClose.onClose && callOnClose) modalToClose.onClose()
-			setCurrentModal(
-				currentModal.filter(infoModal => infoModal.name !== modalName),
-			)
-		}
+		setCurrentModal(prev => {
+			const modalToClose = prev.find(modal => modal.name === modalName)
+			if (modalToClose && modalToClose.onClose && callOnClose)
+				modalToClose.onClose()
+
+			return prev.filter(infoModal => infoModal.name !== modalName)
+		})
 	}
 
 	const onOpenChange = (modalName: string) => {
@@ -33,6 +36,7 @@ export default function useModal() {
 
 	const isModalOpen = (modalName: string) =>
 		currentModal.some(infoModal => infoModal.name === modalName)
+
 	return {
 		currentModal,
 		onOpenChange,
