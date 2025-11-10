ef7bc1dc9516b1e81b07707711c3897156665c95
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-10 16:21:54 -0500

    fix(home-offers): adjust padding and gap in HomeOffers component for improved layout consistency

--- src/sections/home/components/home-offers/home-offers.tsx
+++ src/sections/home/components/home-offers/home-offers.tsx
@@ -4,8 +4,8 @@ import React, { Suspense } from "react"
 
 export default function HomeOffers() {
 	return (
-		<section id="home-offers" className="bg-muted pt-12 pb-20">
-			<div className="flex flex-col gap-12 pt-4">
+		<section id="home-offers" className="bg-muted pt-12 pb-12">
+			<div className="flex flex-col gap-8 pt-4">
 				<div className="text-center space-y-4 px-4">
 					<h2 className="text-3xl lg:text-4xl font-bold text-secondary">
 						Aromas irresistibles a precios especiales
