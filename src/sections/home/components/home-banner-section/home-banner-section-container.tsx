d0a9f3e7624ec0d458a40fe4d4dc561a5762919e
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-10 18:48:19 -0500

    fix(home-banner): remove mobile version to streamline layout and enhance responsiveness

--- src/sections/home/components/home-banner-section/home-banner-section-container.tsx
+++ src/sections/home/components/home-banner-section/home-banner-section-container.tsx
@@ -1,7 +1,5 @@
 import { getMainHomeBanner } from "@/src/lib/services/home-banners"
 import { HomeBannerSectionDekstop } from "@/src/sections/home/components/home-banner-section/components/home-banner-section-dekstop"
-import HomeBannerSectionMobile from "@/src/sections/home/components/home-banner-section/home-banner-section-mobile"
-
 import React from "react"
 
 export default async function HomeBannerSectionContainer() {
