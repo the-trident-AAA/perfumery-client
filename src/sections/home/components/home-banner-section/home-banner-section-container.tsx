fdfba5ff54c7b4857920b6e46f3d64cbcfebd46a
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-12 17:45:08 -0500

    fix(home-banner-section): implement carousel for home banners to enhance visual presentation and user experience

--- src/sections/home/components/home-banner-section/home-banner-section-container.tsx
+++ src/sections/home/components/home-banner-section/home-banner-section-container.tsx
@@ -1,16 +1,34 @@
-import { getMainHomeBanner } from "@/src/lib/services/home-banners"
+import { CarouselItem } from "@/src/components/ui/carousel"
+import StandardCarousel from "@/src/components/ui/standard-carousel/standard-carousel"
+import { getHomeBannersList } from "@/src/lib/services/home-banners"
+import { cn } from "@/src/lib/utils/utils"
 import { HomeBannerSectionDekstop } from "@/src/sections/home/components/home-banner-section/components/home-banner-section-dekstop"
 import React from "react"
 
 export default async function HomeBannerSectionContainer() {
-	const res = await getMainHomeBanner()
+	const res = await getHomeBannersList({})
 
 	if (!res.response || res.error)
-		throw new Error("Problemas en la carga del main home banner")
+		throw new Error("Problemas en la carga de los home banners")
+
+	const homeBanners = res.response
 
 	return (
 		<section id="home-hero">
-			<HomeBannerSectionDekstop homeBanner={res.response} />
+			<StandardCarousel
+				contentClassName="py-0 px-0"
+				navVariant="banner"
+				loop={false}
+				autoPlay
+				autoPlayDelay={20000}
+				showDots
+			>
+				{homeBanners.map((homeBanner, index) => (
+					<CarouselItem key={index} className={cn("basis-full")}>
+						<HomeBannerSectionDekstop homeBanner={homeBanner} />
+					</CarouselItem>
+				))}
+			</StandardCarousel>
 		</section>
 	)
 }
