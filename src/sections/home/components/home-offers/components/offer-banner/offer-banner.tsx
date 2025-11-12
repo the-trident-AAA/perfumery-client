fd10d9a662e98160be96ddb3b453dd41f3eafd59
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-11 23:32:43 -0500

    fix(offer-banner): wrap offer banner in NavigationComponent for improved interactivity and maintain layout consistency

--- src/sections/home/components/home-offers/components/offer-banner/offer-banner.tsx
+++ src/sections/home/components/home-offers/components/offer-banner/offer-banner.tsx
@@ -16,116 +16,121 @@ export default function OfferBanner({
 	offer: { id, name, description, image, offerType, scope, discount },
 }: Props) {
 	return (
-		<article className="w-full h-full relative overflow-hidden sm:rounded-3xl group">
-			{/* Imagen de fondo */}
-			<div className="absolute inset-0">
-				<Image
-					src={image || offerImagePlaceHolder}
-					width={1920}
-					height={600}
-					quality={100}
-					alt={`Oferta ${name}: ${description.slice(0, 60)}...`}
-					className="object-cover w-full h-full transition-all duration-700 group-hover:scale-105"
-				/>
-				<div className="absolute inset-0 bg-gradient-to-b from-black/70 via-black/70 to-black/70" />
-			</div>
+		<NavigationComponent
+			href={paths.perfumes({ offerId: id.toString() }).root}
+		>
+			<article className="w-full h-full relative cursor-pointer overflow-hidden sm:rounded-3xl group">
+				{/* Imagen de fondo */}
+				<div className="absolute inset-0">
+					<Image
+						src={image || offerImagePlaceHolder}
+						width={1920}
+						height={600}
+						quality={100}
+						alt={`Oferta ${name}: ${description.slice(0, 60)}...`}
+						className="object-cover w-full h-full transition-all duration-700 group-hover:scale-105"
+					/>
+					<div className="absolute inset-0 bg-gradient-to-b from-black/70 via-black/70 to-black/70" />
+				</div>
 
-			{/* Contenedor principal */}
-			<div
-				className="
+				{/* Contenedor principal */}
+				<div
+					className="
 				relative z-10 
 				flex flex-col-reverse md:flex-row items-center justify-between 
 				gap-6 sm:gap-8 
 				p-4 sm:p-8 md:p-12 lg:p-16 h-full
 				sm:max-h-[380px] lg:max-h-[380px] 
 			"
-			>
-				{/* Columna izquierda */}
-				<div className="flex flex-col justify-between space-y-4 sm:space-y-4 text-white lg:max-w-2xl">
-					<div className="flex flex-col h-full space-y-2 2xs:space-y-4 sm:space-y-4 text-white">
-						{/* Badge superior */}
-						<div className="flex gap-2 items-center justify-between">
-							<div className="flex items-center gap-2 sm:gap-3 animate-fade-in">
-								<Tag className="h-4 w-4 sm:h-5 sm:w-5 text-yellow-400" />
-								<Badge className="bg-white/10 backdrop-blur-sm border-secondary/30 font-semibold px-3 sm:px-4 py-1 2xs:py-1.5 sm:py-2 text-[9px] 2xs:text-[10px] sm:text-sm shadow-lg">
-									{offerType}
-								</Badge>
+				>
+					{/* Columna izquierda */}
+					<div className="flex flex-col justify-between space-y-4 sm:space-y-4 text-white lg:max-w-2xl">
+						<div className="flex flex-col h-full space-y-2 2xs:space-y-4 sm:space-y-4 text-white">
+							{/* Badge superior */}
+							<div className="flex gap-2 items-center justify-between">
+								<div className="flex items-center gap-2 sm:gap-3 animate-fade-in">
+									<Tag className="h-4 w-4 sm:h-5 sm:w-5 text-yellow-400" />
+									<Badge className="bg-white/10 backdrop-blur-sm border-secondary/30 font-semibold px-3 sm:px-4 py-1 2xs:py-1.5 sm:py-2 text-[9px] 2xs:text-[10px] sm:text-sm shadow-lg">
+										{offerType}
+									</Badge>
+								</div>
+								<div className="flex lg:hidden">
+									<TargetDiscount discount={discount} />
+								</div>
 							</div>
-							<div className="flex lg:hidden">
-								<TargetDiscount discount={discount} />
-							</div>
-						</div>
 
-						{/* Título principal */}
-						<div className="space-y-1 sm:space-y-2 animate-fade-in-up">
-							<h2
-								className="
+							{/* Título principal */}
+							<div className="space-y-1 sm:space-y-2 animate-fade-in-up">
+								<h2
+									className="
 							text-base 2xs:text-lg sm:text-2xl md:text-3xl lg:text-4xl 
 							font-black tracking-tight text-balance
 						"
-							>
-								{name}
-							</h2>
-							<div className="h-0.5 2xs:h-1 w-16 sm:w-24 bg-white rounded-full" />
-						</div>
+								>
+									{name}
+								</h2>
+								<div className="h-0.5 2xs:h-1 w-16 sm:w-24 bg-white rounded-full" />
+							</div>
 
-						{/* Descripción */}
-						<p
-							className="
+							{/* Descripción */}
+							<p
+								className="
 						text-xs 2xs:text-base sm:text-lg md:text-lg 
 						text-gray-200 line-clamp-3 leading-relaxed text-pretty animate-fade-in-up
 					"
-						>
-							{description}
-						</p>
-
-						{/* Alcance */}
-						<div className="flex items-center gap-2 animate-fade-in-up">
-							<Sparkles className="h-3 w-3 sm:h-5 sm:w-5 text-yellow-400" />
-							<p className="text-[10px] 2xs:text-sm sm:text-base md:text-base font-semibold">
-								{scope}
+							>
+								{description}
 							</p>
+
+							{/* Alcance */}
+							<div className="flex items-center gap-2 animate-fade-in-up">
+								<Sparkles className="h-3 w-3 sm:h-5 sm:w-5 text-yellow-400" />
+								<p className="text-[10px] 2xs:text-sm sm:text-base md:text-base font-semibold">
+									{scope}
+								</p>
+							</div>
+						</div>
+
+						{/* Botón CTA */}
+						<div className="sm:pt-4 animate-fade-in-up">
+							<NavigationComponent
+								href={
+									paths.perfumes({ offerId: id.toString() })
+										.root
+								}
+							>
+								<Button
+									size="lg"
+									variant="secondary"
+									className="
+									text-primary px-2 2xs:px-5 sm:px-8 h-auto 2xs:h-9 py-1.5 2xs:py-4 sm:py-6 
+									text-[10px] 2xs:text-sm sm:text-base lg:text-lg font-bold 
+									group/btn shadow-2xl cursor-pointer hover:shadow-secondary/50 
+									transition-all duration-300 transform hover:scale-105 relative overflow-hidden
+								"
+								>
+									<span className="relative z-10 flex items-center gap-1 sm:gap-3">
+										Comprar Ahora
+										<ArrowRight className="size-3 2xs:size-4 sm:size-4 transition-all duration-300 group-hover/btn:translate-x-2" />
+									</span>
+
+									<div className="absolute inset-0 bg-gradient-to-r from-transparent via-white/30 to-transparent transform translate-x-[-100%] group-hover/btn:translate-x-[100%] transition-transform duration-700" />
+								</Button>
+							</NavigationComponent>
 						</div>
 					</div>
 
-					{/* Botón CTA */}
-					<div className="sm:pt-4 animate-fade-in-up">
-						<NavigationComponent
-							href={
-								paths.perfumes({ offerId: id.toString() }).root
-							}
-						>
-							<Button
-								size="lg"
-								variant="secondary"
-								className="
-									text-primary px-2 2xs:px-5 sm:px-8 h-auto py-1.5 2xs:py-4 sm:py-6 
-									text-[10px] 2xs:text-sm sm:text-base lg:text-lg font-bold 
-									group/btn shadow-2xl hover:shadow-secondary/50 
-									transition-all duration-300 transform hover:scale-105 relative overflow-hidden
-								"
-							>
-								<span className="relative z-10 flex items-center gap-1 sm:gap-3">
-									Comprar Ahora
-									<ArrowRight className="size-3 2xs:size-4 sm:size-4 transition-all duration-300 group-hover/btn:translate-x-2" />
-								</span>
-
-								<div className="absolute inset-0 bg-gradient-to-r from-transparent via-white/30 to-transparent transform translate-x-[-100%] group-hover/btn:translate-x-[100%] transition-transform duration-700" />
-							</Button>
-						</NavigationComponent>
+					{/* Columna derecha - Tarjeta de descuento */}
+					<div className="hidden lg:flex">
+						<TargetDiscount discount={discount} />
 					</div>
 				</div>
 
-				{/* Columna derecha - Tarjeta de descuento */}
-				<div className="hidden lg:flex">
-					<TargetDiscount discount={discount} />
-				</div>
-			</div>
-
-			{/* Franjas decorativas */}
-			<div className="absolute top-0 left-0 right-0 h-[2px] sm:h-1 bg-gradient-to-r from-transparent via-secondary to-transparent z-20" />
-			<div className="absolute bottom-0 left-0 right-0 h-[2px] sm:h-1 bg-gradient-to-r from-secondary/50 via-secondary to-secondary/50 z-20" />
-		</article>
+				{/* Franjas decorativas */}
+				<div className="absolute top-0 left-0 right-0 h-[2px] sm:h-1 bg-gradient-to-r from-transparent via-secondary to-transparent z-20" />
+				<div className="absolute bottom-0 left-0 right-0 h-[2px] sm:h-1 bg-gradient-to-r from-secondary/50 via-secondary to-secondary/50 z-20" />
+			</article>
+		</NavigationComponent>
 	)
 }
 
