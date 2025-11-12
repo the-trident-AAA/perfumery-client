7136193af44a39e4aa5a98ca97400a9032c2e0ad
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-12 17:23:26 -0500

    refactor(carousels): replace items and renderCard props with children for improved flexibility and consistency

--- src/sections/home/components/home-offers/list/home-offers-list-carousel.tsx
+++ src/sections/home/components/home-offers/list/home-offers-list-carousel.tsx
@@ -1,7 +1,7 @@
-"use client"
-
+import { CarouselItem } from "@/src/components/ui/carousel"
 import StandardCarousel from "@/src/components/ui/standard-carousel/standard-carousel"
 import { Offer } from "@/src/lib/types/offers"
+import { cn } from "@/src/lib/utils/utils"
 import OfferBanner from "@/src/sections/home/components/home-offers/components/offer-banner/offer-banner"
 import React from "react"
 
@@ -13,16 +13,22 @@ export default function HomeOffersListCarousel({ data }: Props) {
 	return (
 		<div className="w-full px-4 sm:px-12 xl:mx-auto xl:max-w-6xl 2xl:max-w-7xl">
 			<StandardCarousel
-				items={data}
-				itemClassName="basis-full "
 				className="shadow-2xl rounded-3xl"
 				contentClassName="py-0 px-0 "
 				loop={false}
 				autoPlay
 				autoPlayDelay={10000}
 				showDots
-				renderCard={item => <OfferBanner offer={item} />}
-			/>
+			>
+				{data.map((offer, index) => (
+					<CarouselItem
+						key={index}
+						className={cn("pl-2 md:pl-4", "basis-full")}
+					>
+						<OfferBanner key={index} offer={offer} />
+					</CarouselItem>
+				))}
+			</StandardCarousel>
 		</div>
 	)
 }
--- src/sections/home/components/home-promotions-carousel/home-promotions-carousel.tsx
+++ src/sections/home/components/home-promotions-carousel/home-promotions-carousel.tsx
@@ -1,9 +1,8 @@
-"use client"
-
+import { CarouselItem } from "@/src/components/ui/carousel"
 import StandardCarousel from "@/src/components/ui/standard-carousel/standard-carousel"
 import { Perfume } from "@/src/lib/types/perfumes"
+import { cn } from "@/src/lib/utils/utils"
 import PerfumeCard from "@/src/sections/perfumes/components/perfume-card/perfume-card"
-
 import React from "react"
 
 interface Props {
@@ -13,14 +12,23 @@ interface Props {
 export default function HomePromotionsCarousel({ data }: Props) {
 	return (
 		<StandardCarousel
-			items={data}
-			itemClassName="basis-[50%] 2xs:basis-[50%] sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/5 2xl:basis-1/5"
 			loop={false}
 			autoPlay
 			autoPlayDelay={10000}
 			navVariant="spaced"
 			showDots
-			renderCard={item => <PerfumeCard perfume={item} />}
-		/>
+		>
+			{data.map((perfume, index) => (
+				<CarouselItem
+					key={index}
+					className={cn(
+						"pl-2 md:pl-4",
+						"basis-[50%] 2xs:basis-[50%] sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/5 2xl:basis-1/5",
+					)}
+				>
+					<PerfumeCard perfume={perfume} />
+				</CarouselItem>
+			))}
+		</StandardCarousel>
 	)
 }
