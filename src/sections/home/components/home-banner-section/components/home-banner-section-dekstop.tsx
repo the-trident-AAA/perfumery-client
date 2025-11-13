2e3dc505ec91482026dfc82e950a892bbdf26ad7
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-13 16:05:42 -0500

    fix(home-banner-section): update padding and text sizes for improved layout and readability

--- src/sections/home/components/home-banner-section/components/home-banner-section-dekstop.tsx
+++ src/sections/home/components/home-banner-section/components/home-banner-section-dekstop.tsx
@@ -13,7 +13,7 @@ interface HomeBannerProps {
 
 export function HomeBannerSectionDekstop({ homeBanner }: HomeBannerProps) {
 	return (
-		<div className="relative min-h-[200px] 2xs:min-h-[400px] 2xl:min-h-[400px] py-4 2xs:py-8 2xl:py-14 w-full overflow-hidden">
+		<div className="relative min-h-[250px] 2xs:min-h-[400px] 2xl:min-h-[400px] py-6 2xs:py-8 2xl:py-14 w-full overflow-hidden">
 			<div className={cn("absolute inset-0")}>
 				<Image
 					src={homeBanner.image}
@@ -28,24 +28,24 @@ export function HomeBannerSectionDekstop({ homeBanner }: HomeBannerProps) {
 			</div>
 
 			<div className="relative z-10 h-full lg:max-w-6xl 2xl:max-w-7xl mx-auto px-6 md:px-8 flex flex-col justify-center">
-				<div className="max-w-3xl space-y-2 2xs:space-y-6">
+				<div className="max-w-3xl space-y-3 2xs:space-y-6">
 					<div className="space-y-1 2xs:space-y-2">
-						<h1 className="text-lg 2xs:text-4xl md:text-5xl font-bold text-white">
+						<h1 className="text-xl 2xs:text-4xl md:text-5xl font-bold text-white">
 							{homeBanner.title}
 						</h1>
-						<p className="text-xs 2xs:text-lg md:text-xl text-white/90 leading-relaxed max-w-2xl">
+						<p className="text-base 2xs:text-lg md:text-xl text-white/90 leading-relaxed max-w-2xl">
 							{homeBanner.description}
 						</p>
 					</div>
 
 					{homeBanner.statisticalTips.length > 0 && (
-						<div className="grid grid-cols-3 items-center">
+						<div className="grid grid-cols-3 w-full items-center">
 							{homeBanner.statisticalTips.map((tip, i) => (
 								<div key={i} className="text-center text-white">
-									<div className="text-xs 2xs:text-lg">
+									<div className="text-sm 2xs:text-lg">
 										{tip.statistics}
 									</div>
-									<div className="text-xs 2xs:text-2xl font-bold">
+									<div className="text-sm 2xs:text-2xl font-bold">
 										{tip.info}
 									</div>
 								</div>
@@ -56,7 +56,7 @@ export function HomeBannerSectionDekstop({ homeBanner }: HomeBannerProps) {
 					<NavigationComponent href={paths.perfumes().root}>
 						<Button
 							variant={"secondary"}
-							className="group bg-secondary text-primary text-xs 2xs:text-base sm:text-base font-bold px-1 2xs:px-4 sm:px-4 py-1.5 2xs:py-2.5 sm:py-2.5 h-auto cursor-pointer rounded-full shadow-2xl transition-all duration-300 transform hover:scale-105 hover:bg-secondary"
+							className="group bg-secondary text-primary text-sm 2xs:text-base sm:text-base font-bold px-1 2xs:px-4 sm:px-4 py-1.5 2xs:py-2.5 sm:py-2.5 h-auto cursor-pointer rounded-full shadow-2xl transition-all duration-300 transform hover:scale-105 hover:bg-secondary"
 						>
 							<ShoppingBag className="size-4 2xs:size-4 2xs:mr-3" />
 							Explorar Colección
@@ -72,7 +72,7 @@ export function HomeBannerSectionDekstop({ homeBanner }: HomeBannerProps) {
 									className="flex items-center gap-2 text-white"
 								>
 									<div className="w-2 h-2 bg-green-400 rounded-full animate-pulse" />
-									<span className="text-[9px] 2xs:text-xs lg:text-sm">
+									<span className="text-[11px] 2xs:text-xs lg:text-sm">
 										{tip}
 									</span>
 								</div>
