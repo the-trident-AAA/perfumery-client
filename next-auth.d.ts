0b002a6fce15357ef2a68993385d62c8042fe101
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-14 16:22:01 -0500

    feat(next-auth): enhance type definitions for User and Session interfaces, adding optional properties and backendUser structure

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,22 +1,38 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 
 declare module "next-auth" {
-	interface User {
-		accessToken: string
-		id: string
-		username?: string
-		email?: string
-		shopCartId?: string
+	interface Session {
+		accessToken?: string
+		user: {
+			id: string
+			username: string
+			email: string
+			shopCartId: string
+		} & DefaultSession["user"]
 	}
 
-	interface Session {
+	interface User {
+		id: string
+		username: string
+		email: string
 		accessToken: string
-		user: User
+		shopCartId: string
 	}
 }
 
 declare module "next-auth/jwt" {
 	interface JWT {
-		accessToken: string
+		id?: string
+		username?: string
+		email?: string
+		accessToken?: string
+		shopCartId?: string
+		backendUser?: {
+			id: string
+			username: string
+			email: string
+			accessToken: string
+			shopCartId: string
+		}
 	}
 }
