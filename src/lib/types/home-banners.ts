ed30734aa270f9bad76e7923930cca923ece6f4c
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-14 13:06:16 -0500

    feat(home-banners): add buttonText and filters properties to HomeBanner interfaces for enhanced functionality

--- src/lib/types/home-banners.ts
+++ src/lib/types/home-banners.ts
@@ -2,24 +2,26 @@ export interface HomeBanner {
 	id: string
 	title: string
 	description: string
+	buttonText: string
 	image: string
 	statisticalTips: {
 		statistics: string
-
 		info: string
 	}[]
 	infoTips: string[]
+	filters: { name: string; value: string }[]
 }
 
 export interface HomeBannerDetails {
 	id: string
 	title: string
 	description: string
+	buttonText: string
 	image: string
 	statisticalTips: {
 		statistics: string
-
 		info: string
 	}[]
 	infoTips: string[]
+	filters: { name: string; value: string }[]
 }
