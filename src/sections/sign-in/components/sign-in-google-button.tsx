473c9885905bb1905e2171c2a1e3f55d0ef068d2
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-14 17:28:07 -0500

    feat(sign-in): add Google sign-in button component with success notification and navigation

---
+++ src/sections/sign-in/components/sign-in-google-button.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { Button } from "@/src/components/ui/button"
+import { paths } from "@/src/lib/routes/paths"
+import useGoogleSignIn from "@/src/sections/sign-in/form/hooks/use-google-sign-in"
+import { FcGoogle } from "react-icons/fc"
+import { toast } from "react-toastify"
+
+export default function GoogleButton() {
+	const { signInWithGoogle, loading: loadingGoogle } = useGoogleSignIn({
+		onSignInAction: () => {
+			toast.success("Inicio de sesión con Google exitoso")
+			window.location.href = paths.home.root
+		},
+	})
+	return (
+		<Button
+			variant="outline"
+			className="w-full flex items-center gap-2"
+			disabled={loadingGoogle}
+			onClick={() => {
+				signInWithGoogle()
+			}}
+		>
+			<FcGoogle size={20} />
+			Continuar con Google
+		</Button>
+	)
+}
