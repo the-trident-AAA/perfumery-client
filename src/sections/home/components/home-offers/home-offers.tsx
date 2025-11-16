80f23213fc194dd315ae096701d687440f8e2de4
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-16 18:20:07 -0500

    fix(home-offers): increase bottom padding for improved layout consistency

--- src/sections/home/components/home-offers/home-offers.tsx
+++ src/sections/home/components/home-offers/home-offers.tsx
@@ -4,7 +4,7 @@ import React, { Suspense } from "react"
 
 export default function HomeOffers() {
 	return (
-		<section id="home-offers" className="bg-muted pt-1 pb-16">
+		<section id="home-offers" className="bg-muted pt-1 pb-20 sm:pb-12">
 			<div className="pt-4">
 				<Suspense
 					fallback={
