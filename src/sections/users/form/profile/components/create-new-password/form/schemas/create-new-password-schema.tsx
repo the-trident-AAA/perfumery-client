8d57cb70bea085fab1f148ef41e6fae167c16748
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-16 23:42:04 -0500

    feat(create-new-password-schema): add confirmPassword field and validation to ensure passwords match

--- src/sections/users/form/profile/components/create-new-password/form/schemas/create-new-password-schema.tsx
+++ src/sections/users/form/profile/components/create-new-password/form/schemas/create-new-password-schema.tsx
@@ -2,10 +2,32 @@ import { z } from "zod"
 
 export interface CreateNewPassword {
 	newPassword: string
+	confirmPassword: string
 }
 
-export const createNewPasswordSchema = z.object({
-	newPassword: z.string().refine(val => val.length === 0 || val.length >= 8, {
-		message: "La contraseña debe tener al menos 8 caracteres",
-	}),
-})
+export const createNewPasswordSchema = z
+	.object({
+		newPassword: z
+			.string()
+			.refine(val => val.length === 0 || val.length >= 8, {
+				message: "La contraseña debe tener al menos 8 caracteres",
+			}),
+		confirmPassword: z
+			.string()
+			.refine(val => val.length === 0 || val.length >= 8, {
+				message: "La contraseña debe tener al menos 8 caracteres",
+			}),
+	})
+	.refine(
+		data => {
+			// Si se ingresa nueva contraseña, la confirmación debe coincidir
+			if (data.newPassword.length > 0) {
+				return data.newPassword === data.confirmPassword
+			}
+			return true
+		},
+		{
+			message: "Las contraseñas no coinciden",
+			path: ["confirmPassword"], // Esto hace que el error aparezca en el campo confirmPassword
+		},
+	)
